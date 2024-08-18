ebf5624adbf2de2f201e80fd2abcc7585d39bd58
Nat <1991.ntm@gmail.com>
2024-08-18 11:04:41 -0700

    adding robot

---
+++ day6.py
@@ -0,0 +1,13 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds/menus/reeborg_intro_en.json&name=Alone&url=worlds/tutorial_en/alone.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+move()
+turn_left()
+move()
+move()
+turn_right()
+move()
