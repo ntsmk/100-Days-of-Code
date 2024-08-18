fd114b088fbf8f9ecbc9a9558066ef87932e6045
Nat <1991.ntm@gmail.com>
2024-08-18 11:45:38 -0700

    adding jump

--- day6.py
+++ day6.py
@@ -11,3 +11,26 @@ move()
 move()
 turn_right()
 move()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+jump()
+jump()
+jump()
+jump()
+jump()
+jump()
