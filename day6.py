95008f74e7ba9b8ec8ebad82a9f1512032cc4ca6
Nat <1991.ntm@gmail.com>
2024-08-18 12:01:01 -0700

    adding for loop

--- day6.py
+++ day6.py
@@ -34,3 +34,26 @@ jump()
 jump()
 jump()
 jump()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+#x = 0
+#while x < 6:
+#    jump()
+#    x += 1
+
+for step in range(6):
+    jump()
