b3356aca10009c3eba7f17fe8821e54f4bb46142
ntsmk <1991.ntm@gmail.com>
2024-08-21 10:27:43 -0700

    added some

--- day7.py
+++ day7.py
@@ -3,13 +3,21 @@ word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 chosen_word = random.choice(word_list)
+print(chosen_word)
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("guess a letter\n").lower()
+guess = input("guess a letter: ").lower()
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
 #  is, "Wrong" if it's not.
-if guess in chosen_word:
-    print("Right")
-else:
-    print("Wrong")
+
+# if guess in chosen_word:
+#     print("Right")
+# else:
+#     print("Wrong")
+
+for i in chosen_word:
+    if i == guess:
+        print("Right")
+    else:
+        print("Wrong")
