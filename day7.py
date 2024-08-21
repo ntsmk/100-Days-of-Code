b012fdcfbc49ea335d634666465881eea889aba1
ntsmk <1991.ntm@gmail.com>
2024-08-21 12:15:19 -0700

    update part 2

--- day7.py
+++ day7.py
@@ -7,29 +7,37 @@ print(chosen_word)
 # TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
 
 guess = input("Guess a letter: ").lower()
-placeholder_list = []
+# placeholder_list = []
+# for i in chosen_word:
+#     placeholder_list.append("_")
+# placeholder = ''.join(placeholder_list)
+# print(placeholder)
+
+placeholder = ""
 for i in chosen_word:
-    placeholder_list.append("_")
-placeholder = ''.join(placeholder_list)
+    placeholder += "_"
 print(placeholder)
 
 # TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
-
+display = ""
 for letter in chosen_word:
     if letter == guess:
         print("Right")
+        display += letter
     else:
         print("Wrong")
-
-display_list = []
-
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-        display_list.append(letter)
-    else:
-        print("Wrong")
-        display_list.append("_")
-
-display = ''.join(display_list)
+        display += "_"
 print(display)
+
+# display_list = []
+#
+# for letter in chosen_word:
+#     if letter == guess:
+#         print("Right")
+#         display_list.append(letter)
+#     else:
+#         print("Wrong")
+#         display_list.append("_")
+#
+# display = ''.join(display_list)
+# print(display)
