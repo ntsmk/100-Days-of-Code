a7157803ea5d38081bab28287595b69a9cb09420
Nat <1991.ntm@gmail.com>
2024-08-20 22:04:29 -0700

    added part 2

--- day6.py
+++ day6.py
@@ -114,3 +114,19 @@ while not at_goal():
             move()
     else:
         turn_left()
+
+# part 2
+    def turn_right():
+        turn_left()
+        turn_left()
+        turn_left()
+
+
+    while not at_goal():
+        if right_is_clear():
+            turn_right()
+            move()
+        elif front_is_clear():
+            move()
+        else:
+            turn_left()
