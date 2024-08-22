ff70c8d80dfe10617ec781018ab3cc130068db3b
ntsmk <1991.ntm@gmail.com>
2024-08-22 11:53:46 -0700

    added step 4

--- day7.py
+++ day7.py
@@ -1,4 +1,60 @@
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
@@ -9,22 +65,39 @@ word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
 print(placeholder)
-matched_letters = []
 
-# TODO-1: - Use a while loop to let the user guess again.
-display = placeholder
-while "_" in display:
+game_over = False
+correct_letters = []
+lives = 6
+
+while not game_over:
     guess = input("Guess a letter: ").lower()
+
     display = ""
 
     for letter in chosen_word:
         if letter == guess:
             display += letter
-            matched_letters.append(letter)
-        elif letter in matched_letters:
+            correct_letters.append(guess)
+            lives += 1
+        elif letter in correct_letters:
             display += letter
+            lives += 1
         else:
             display += "_"
 
-    print(display)
+        if letter != guess:
+            lives -= 1
 
+    print(display)
+    print(f"your life is {lives} left")
+    print(stages[lives])
+
+    # TODO-2: - If guess is not a letter in the chosen_word, Then reduce 'lives' by 1.
+    if lives <= 0:
+        game_over = True
+        print("You lose.")
+
+    if "_" not in display:
+        game_over = True
+        print("You win.")
