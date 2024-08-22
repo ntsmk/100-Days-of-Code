4250a79a3951ff399e13bbf686f165afba3875ca
Nat <1991.ntm@gmail.com>
2024-08-21 23:00:39 -0700

    adding part 3

--- day7.py
+++ day7.py
@@ -1,47 +1,3 @@
-# import random
-# word_list = ["aardvark", "baboon", "camel"]
-#
-# chosen_word = random.choice(word_list)
-# print(chosen_word)
-#
-# # TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
-#
-# guess = input("Guess a letter: ").lower()
-# # placeholder_list = []
-# # for i in chosen_word:
-# #     placeholder_list.append("_")
-# # placeholder = ''.join(placeholder_list)
-# # print(placeholder)
-#
-# placeholder = ""
-# for i in chosen_word:
-#     placeholder += "_"
-# print(placeholder)
-#
-# # TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
-# display = ""
-# for letter in chosen_word:
-#     if letter == guess:
-#         print("Right")
-#         display += letter
-#     else:
-#         print("Wrong")
-#         display += "_"
-# print(display)
-
-# display_list = []
-#
-# for letter in chosen_word:
-#     if letter == guess:
-#         print("Right")
-#         display_list.append(letter)
-#     else:
-#         print("Wrong")
-#         display_list.append("_")
-#
-# display = ''.join(display_list)
-# print(display)
-
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
@@ -53,21 +9,26 @@ word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
 print(placeholder)
+matched_letters = []
 
+# still in progress
 # TODO-1: - Use a while loop to let the user guess again.
-
 display = ""
+
 
 while "_" in display:
     guess = input("Guess a letter: ").lower()
 
+# todo2 is done
 # TODO-2: Change the for loop so that you keep the previous correct letters in display.
-
     for letter in chosen_word:
         if letter == guess:
             display += letter
-        elif
+            matched_letters.append(letter)
+        elif letter in matched_letters:
+            display += letter
         else:
             display += "_"
 
     print(display)
+
