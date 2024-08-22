0260cbf63044c9d89a6797140e75763fdd4417c3
ntsmk <1991.ntm@gmail.com>
2024-08-22 15:51:13 -0700

    step 5 in progress

--- day7.py
+++ day7.py
@@ -1,61 +1,11 @@
 import random
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-word_list = ["aardvark", "baboon", "camel"]
+import hangman_art
+import hangman_words
+
+lives = 6
+word_list = hangman_words.word_list
+
+print(hangman_art.logo)
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -64,40 +14,42 @@ placeholder = ""
 word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
-print(placeholder)
+print("Word to guess: " + placeholder)
 
 game_over = False
 correct_letters = []
-lives = 6
 
 while not game_over:
+    print(f"**************************** {lives}/6 LIVES LEFT ****************************")
     guess = input("Guess a letter: ").lower()
 
+    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+
     display = ""
-    if guess not in chosen_word:
-        lives -= 1
 
     for letter in chosen_word:
-
         if letter == guess:
             display += letter
             correct_letters.append(guess)
-
         elif letter in correct_letters:
             display += letter
-
         else:
             display += "_"
 
-    print(display)
-    print(f"your life is {lives} left")
-    print(stages[lives])
+    print("Word to guess: " + display)
 
-    # TODO-2: - If guess is not a letter in the chosen_word, Then reduce 'lives' by 1.
-    if lives <= 0:
-        game_over = True
-        print("You lose.")
+    # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+    #  e.g. You guessed d, that's not in the word. You lose a life.
+
+    if guess not in chosen_word:
+        lives -= 1
+
+        if lives == 0:
+            game_over = True
+            print(f"*********************** It was {chosen_word} ! YOU LOSE, NEXT TIME **********************")
 
     if "_" not in display:
         game_over = True
-        print("You win.")
+        print("**************************** YOU WIN ****************************")
+
+    print(hangman_art.stages[lives])
