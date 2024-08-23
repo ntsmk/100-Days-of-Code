a3243c0770f23081540d1e2fe50d1f0951dd44e2
Nat <1991.ntm@gmail.com>
2024-08-22 23:03:57 -0700

    adding step 1

--- day8.py
+++ day8.py
@@ -1,28 +1,29 @@
-def calculate_love_score(name1, name2):
-    name = name1 + name2
-    count1 = 0
-    for i in name.lower():
-        if "t" in i:
-            count1 += 1
-        elif "r" in i:
-            count1 += 1
-        elif "u" in i:
-            count1 += 1
-        elif "e" in i:
-            count1 += 1
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z']
 
-    count2 = 0
-    for i in name.lower():
-        if "l" in i:
-            count2 += 1
-        elif "o" in i:
-            count2 += 1
-        elif "v" in i:
-            count2 += 1
-        elif "e" in i:
-            count2 += 1
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+def encrypt(original_text,shift_amount):
+    original_text = text
+    shift_amount = shift
+    thislist1 = []
+    thislist2 = []
+    encrypted_word = ""
 
-    print(str(count1) + str(count2))
+    for i in text:
+        thislist1.append(alphabet.index(i))
+        # z = 25
 
+    for i in thislist1:
+        i += shift
+        thislist2.append(i)
+        # shifted_z = more than 26
 
-calculate_love_score("Angela Yu", "Jack Bauer")
+    for i in thislist2:
+        if i >= 26:
+            i -= 26
+        encrypted_word += alphabet[i]
+
+    print(encrypted_word)
+
+encrypt("a",1)
