8a2c4b5bab19b56c0d64e6c9f7c2070853c4055d
ntsmk <1991.ntm@gmail.com>
2024-08-23 12:00:03 -0700

    finished cipher 2

--- day8.py
+++ day8.py
@@ -1,28 +1,44 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-def encrypt(original_text,shift_amount):
-    original_text = text
-    shift_amount = shift
-    thislist1 = []
-    thislist2 = []
-    encrypted_word = ""
 
-    for i in text:
-        thislist1.append(alphabet.index(i))
-        # z = 25
 
-    for i in thislist1:
-        i += shift
-        thislist2.append(i)
-        # shifted_z = more than 26
+# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
+#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
-    for i in thislist2:
-        if i >= 26:
-            i -= 26
-        encrypted_word += alphabet[i]
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+    print(f"Here is the encoded result: {cipher_text}")
+
+
+#encrypt(original_text=text, shift_amount=shift)
+
+
+def decrypt(original_text, shift_amount):
+    decipher_text = ""
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) - shift_amount
+        shifted_position %= len(alphabet)
+        decipher_text += alphabet[shifted_position]
+    print(f"Here is the encoded result: {decipher_text}")
+
+#decrypt(original_text=text, shift_amount=shift)
+
+
+def caesar():
+    if direction == "encode":
+        encrypt(text,shift)
+    if direction == "decode":
+        decrypt(text,shift)
+
+caesar()
+
 
     print(encrypted_word)
 
