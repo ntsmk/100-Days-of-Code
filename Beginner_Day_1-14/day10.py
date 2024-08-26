9ee73e21a9b1beb58dbadd7f217b07fd368e476b
Nat <1991.ntm@gmail.com>
2024-08-25 20:08:47 -0700

    adding calc project

--- Beginner_Day_1-14/day10.py
+++ Beginner_Day_1-14/day10.py
@@ -1,12 +1,46 @@
-def is_leap_year(year):
-    # Write your code here.
-    # Don't change the function name.
-    if year % 4 == 0 and year % 400 == 0:
-        return True
-    elif year % 100 == 0:
-        return False
-    else:
-        return False
+def add(n1, n2):
+    return n1 + n2
 
-print(is_leap_year(1989))
+# TODO: Write out the other 3 functions - subtract, multiply and divide.
+def subtract(n1, n2):
+    return n1 - n2
 
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+# TODO: Add these 4 functions into a dictionary as the values. Keys = "+", "-", "*", "/"
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+# TODO: Use the dictionary operations to perform the calculations. Multiply 4 * 8 using the dictionary.
+op = "*"
+print(operations[op](4, 8))
+
+
+
+
+# first_number = input("What's the first number?: ")
+# operation = input("+\n-\n*\n/\nPick an operation: ")
+# second_number = input("What's the second number?")
+#
+#
+# if operation == "+":
+#     answer = add(first_number, second_number)
+#     print(f"{first_number} {operation} {second_number} = {answer}")
+# elif operation == "-":
+#     answer = subtract(first_number, second_number)
+#     print(f"{first_number} {operation} {second_number} = {answer}")
+# elif operation == "*":
+#     answer = multiply(first_number, second_number)
+#     print(f"{first_number} {operation} {second_number} = {answer}")
+# elif operation == "/":
+#     answer = divide(first_number, second_number)
+#     print(f"{first_number} {operation} {second_number} = {answer}")
