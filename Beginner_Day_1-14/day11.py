2ef9fcb5f3a3293b11832f8cd0b425b7f603ff19
Nat <1991.ntm@gmail.com>
2024-08-26 20:04:38 -0700

    adding first ver

---
+++ Beginner_Day_1-14/day11.py
@@ -0,0 +1,8 @@
+import random
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+your_cards = [random.choice(cards),random.choice(cards)]
+current_score = your_cards[0]+your_cards[1]
+com_first_card = random.choice(cards)
+print(your_cards)
+print(current_score)
