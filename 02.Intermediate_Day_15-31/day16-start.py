7fb7055821204b8507059ed976fc650d7cfb2d8d
Nat <1991.ntm@gmail.com>
2024-09-05 22:06:28 -0700

    adding pretty table

--- 02.Intermediate_Day_15-31/day16-start.py
+++ 02.Intermediate_Day_15-31/day16-start.py
@@ -1,12 +1,20 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# tom = Turtle()
+# my_screen = Screen()
+# tom.shape("turtle")
+# tom.color("green")
+# tom.forward(100)
+# tom.back(300)
+# tom.clear()
+#
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-tom = Turtle()
-my_screen = Screen()
-tom.shape("turtle")
-tom.color("green")
-tom.forward(100)
-tom.back(300)
-tom.clear()
-
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from prettytable import PrettyTable
+table = PrettyTable()
+print(table)
+table.add_column("Pokemon Name",["Pikachu", "Pichu", "Raichu"])
+table.add_column("Type",["Electric", "Fire", "Water"])
+table.align = "l"
+print(table)
