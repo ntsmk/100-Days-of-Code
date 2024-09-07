4bdf25bf34abe55ca996b872f25df7864851224e
Nat <1991.ntm@gmail.com>
2024-09-06 21:37:03 -0700

    done

--- 02.Intermediate_Day_15-31/day16-coffee.py
+++ 02.Intermediate_Day_15-31/day16-coffee.py
@@ -9,17 +9,32 @@ m = Menu()
 cm = CoffeeMaker()
 mm = MoneyMachine()
 
-item = MenuItem("latte", 100,200,30,2)
-print(item.ingredients)
+# item = MenuItem("latte", 100,200,30,2)
+# print(item.ingredients)
 
-print(m.get_items()) # latte/espresso/cappuccino/
-print(m.find_drink("latte").ingredients) # {'water': 200, 'milk': 150, 'coffee': 24}
+# print(m.get_items()) # latte/espresso/cappuccino/
+# print(m.find_drink("latte").ingredients) # {'water': 200, 'milk': 150, 'coffee': 24}
 
-cm.report()
-print(cm.is_resource_sufficient(m.find_drink("latte"))) # true
-cm.make_coffee(m.find_drink("latte")) # Here is your latte ☕️. Enjoy!
+# cm.report()
+# print(cm.is_resource_sufficient(m.find_drink("latte"))) # true
+# cm.make_coffee(m.find_drink("latte")) # Here is your latte ☕️. Enjoy!
 
-mm.report()
-enough_or_not = mm.make_payment(2)
-print(enough_or_not) # true or false
+# mm.report()
+# enough_or_not = mm.make_payment(2)
+# print(enough_or_not) # true or false
+
+machine_on = True
+while machine_on:
+    command = input(f"Welcome. What would you like? ({m.get_items()}): ")
+
+    if command == 'report':
+        cm.report()
+        mm.report()
+    elif command == 'off':
+        machine_on = False
+    else:
+        drink = m.find_drink(command)
+        if cm.is_resource_sufficient(drink):
+            if mm.make_payment(drink.cost):
+                cm.make_coffee(drink)
 
