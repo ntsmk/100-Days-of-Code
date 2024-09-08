2ba7e065bc7e91f25e2ba8387ef45fe9d18f1320
Nat <1991.ntm@gmail.com>
2024-09-07 23:58:48 -0700

    adding score

--- 02.Intermediate_Day_15-31/day17/quiz_brain.py
+++ 02.Intermediate_Day_15-31/day17/quiz_brain.py
@@ -2,6 +2,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_questions(self):
         if self.question_number == len(self.question_list):
@@ -18,6 +19,8 @@ class QuizBrain:
     def check_answer(self, choice, correct_answer):
         if choice.lower() == correct_answer.lower():
             print("you are right!")
+            self.score += 1
         else:
             print("you are wrong")
         print(f"correct answer was {correct_answer}")
+        print(f"current score: {self.score}")
