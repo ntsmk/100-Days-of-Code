f9bda75295b91ff699504b50d2b528cb748e5f1f
Nat <1991.ntm@gmail.com>
2024-09-08 14:53:23 -0700

    fixing the code

--- 02.Intermediate_Day_15-31/day18-start.py
+++ 02.Intermediate_Day_15-31/day18-start.py
@@ -1,25 +1,27 @@
 from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color('#f9d910')
+
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(100)
+
+# import turtle as t
+# from random import random
 #
-# timmy_the_turtle = Turtle()
-# timmy_the_turtle.shape("turtle")
-# timmy_the_turtle.color('#f9d910')
-# timmy_the_turtle.forward(100)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.forward(100)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.forward(100)
-# timmy_the_turtle.right(90)
-# timmy_the_turtle.forward(100)
-
-import turtle as t
-from random import random
-
-for i in range(100):
-    steps = int(random() * 100)
-    angle = int(random() * 360)
-    t.right(angle)
-    t.fd(steps)
-    t.speed(8)
+# for i in range(100):
+#     steps = int(random() * 100)
+#     angle = int(random() * 360)
+#     t.right(angle)
+#     t.fd(steps)
+#     t.speed(8)
 
 screen = Screen()
 screen.exitonclick()
