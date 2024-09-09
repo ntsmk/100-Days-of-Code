e149b77566d3ab89ddd22c9c53efca5143488de4
ntsmk <1991.ntm@gmail.com>
2024-09-09 16:55:06 -0700

    adding random line

--- 02.Intermediate_Day_15-31/day18-start.py
+++ 02.Intermediate_Day_15-31/day18-start.py
@@ -1,9 +1,16 @@
 from turtle import Turtle, Screen
-import random
+from random import random
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color('#f9d910')
+
+def change_color():
+    R = random()
+    B = random()
+    G = random()
+
+    timmy_the_turtle.color(R, G, B)
 
 # for _ in range(4):
 #     timmy_the_turtle.forward(100)
@@ -14,15 +21,15 @@ timmy_the_turtle.color('#f9d910')
 # timmy_the_turtle.right(90)
 # timmy_the_turtle.forward(100)
 
-# import turtle as t
 # from random import random
-#
-# for i in range(100):
-#     steps = int(random() * 100)
-#     angle = int(random() * 360)
-#     t.right(angle)
-#     t.fd(steps)
-#     t.speed(8)
+
+for i in range(100):
+    steps = int(random() * 100)
+    # angle = int(random() * 360)
+    change_color()
+    timmy_the_turtle.right(90)
+    timmy_the_turtle.fd(steps)
+    timmy_the_turtle.speed(8)
 
 # for _ in range(10):
 #     timmy_the_turtle.penup()
@@ -30,18 +37,13 @@ timmy_the_turtle.color('#f9d910')
 #     timmy_the_turtle.pendown()
 #     timmy_the_turtle.forward(10)
 
-def change_color():
-    R = random.random()
-    B = random.random()
-    G = random.random()
 
-    timmy_the_turtle.color(R, G, B)
 
-for i in range(3,9):
-    for _ in range(i):
-        timmy_the_turtle.forward(100)
-        timmy_the_turtle.right(360/i)
-    change_color()
+# for i in range(3,9):
+#     for _ in range(i):
+#         timmy_the_turtle.forward(100)
+#         timmy_the_turtle.right(360/i)
+#     change_color()
 
 screen = Screen()
 screen.exitonclick()
