25bbf93fb9107ca1dbb90b77bd6f68f347be5a14
ntsmk <1991.ntm@gmail.com>
2024-09-11 14:11:30 -0700

    finished for loop

--- 02.Intermediate_Day_15-31/day19/main.py
+++ 02.Intermediate_Day_15-31/day19/main.py
@@ -23,37 +23,44 @@ colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 # screen.onkey(clockwise, "d")
 # screen.onkey(clear, "c")
 # screen.listen()
+thislist = ["t1", "t2", "t3", "t4", "t5", "t6"]
+for i in range(6):
+    thislist[i] = Turtle(shape="turtle")
+    thislist[i].color(colors[i])
+    thislist[i].penup()
+    thislist[i].goto(x=-230, y=-100+40*i)
 
-t1 = Turtle(shape="turtle")
-t1.color(colors[0])
-t1.penup()
-t1.goto(x=-230,y=-100)
-
-t2 = Turtle(shape="turtle")
-t2.color(colors[1])
-t2.penup()
-t2.goto(x=-230,y=-60)
-
-t3 = Turtle(shape="turtle")
-t3.color(colors[2])
-t3.penup()
-t3.goto(x=-230,y=-20)
-
-t4 = Turtle(shape="turtle")
-t4.color(colors[3])
-t4.penup()
-t4.goto(x=-230,y=20)
-
-t5 = Turtle(shape="turtle")
-t5.color(colors[4])
-t5.penup()
-t5.goto(x=-230,y=60)
-
-t6 = Turtle(shape="turtle")
-t6.color(colors[5])
-t6.penup()
-t6.goto(x=-230,y=100)
-
+#
+# t1 = Turtle(shape="turtle")
+# t1.color(colors[0])
+# t1.penup()
+# t1.goto(x=-230,y=-100) 1 * -100
+#
+# t2 = Turtle(shape="turtle")
+# t2.color(colors[1])
+# t2.penup()
+# t2.goto(x=-230,y=-60) 2 * -30
+#
+# t3 = Turtle(shape="turtle")
+# t3.color(colors[2])
+# t3.penup()
+# t3.goto(x=-230,y=-20) 3 * -7
+#
+# t4 = Turtle(shape="turtle")
+# t4.color(colors[3])
+# t4.penup()
+# t4.goto(x=-230,y=20) 4 * 5
+#
+# t5 = Turtle(shape="turtle")
+# t5.color(colors[4])
+# t5.penup()
+# t5.goto(x=-230,y=60) 5 * 10
+#
+# t6 = Turtle(shape="turtle")
+# t6.color(colors[5])
+# t6.penup()
+# t6.goto(x=-230,y=100) 6 * 17
+#
 
 
 
