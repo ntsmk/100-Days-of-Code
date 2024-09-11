db354439a331bfcad6bacf91ba7eda40de5f459b
ntsmk <1991.ntm@gmail.com>
2024-09-10 17:19:45 -0700

    creating color list

--- 02.Intermediate_Day_15-31/day18-final-project/main.py
+++ 02.Intermediate_Day_15-31/day18-final-project/main.py
@@ -1,25 +1,18 @@
-import colorgram
+# import colorgram
+#
+# # Extract 6 colors from an image.
+# colors = colorgram.extract('img.jpg', 30)
+#
+# def getting_color(color):
+#     color_list = []
+#     for color_from_list in color:
+#         r = color_from_list.rgb.r
+#         g = color_from_list.rgb.g
+#         b = color_from_list.rgb.b
+#         color_list.append((r, g, b))
+#     return color_list
+#
+# a = getting_color(colors)
+# print(a)
 
-# Extract 6 colors from an image.
-colors = colorgram.extract('img.jpg', 10)
-
-def getting_color(color):
-    color_list = []
-    for color_from_list in color:
-        r = color_from_list.rgb.r
-        g = color_from_list.rgb.g
-        b = color_from_list.rgb.b
-        color_list.append((r, g, b))
-    return color_list
-
-a = getting_color(colors)
-print(a)
-
-
-# for color in colors:
-#     # print(i.rgb.r)
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     print(r, g, b)
-
+color_list = [(246, 243, 239), (247, 242, 244), (203, 165, 108), (239, 245, 240), (152, 74, 48), (234, 238, 243), (52, 93, 124), (170, 153, 41), (223, 202, 136), (137, 32, 21), (132, 163, 185), (47, 121, 88), (198, 91, 72), (15, 99, 73), (70, 47, 39), (147, 179, 148), (98, 73, 75), (162, 142, 157), (234, 175, 164), (55, 46, 49), (184, 205, 172), (24, 81, 87), (38, 61, 74), (142, 22, 25), (85, 146, 126), (45, 65, 85), (175, 91, 94), (214, 177, 183), (18, 70, 64), (109, 125, 151)]
