f433fc5a9285e00a0c4abb674f8b69a0c9773de3
ntsmk <1991.ntm@gmail.com>
2024-09-11 12:42:44 -0700

    adding 6 turtles

--- 02.Intermediate_Day_15-31/day19/main.py
+++ 02.Intermediate_Day_15-31/day19/main.py
@@ -1,30 +1,60 @@
 from turtle import Turtle, Screen
 
-t = Turtle()
+
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="make your bet", prompt="which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
-def forwards():
-    t.forward(10)
+# def forwards():
+#     t.forward(10)
+# def backwards():
+#     t.backward(10)
+# def clockwise():
+#     t.right(10)
+# def counter_clockwise():
+#     t.left(10)
+# def clear():
+#     t.clear()
+#
+# screen.onkey(forwards, "w")
+# screen.onkey(backwards, "s")
+# screen.onkey(counter_clockwise, "a")
+# screen.onkey(clockwise, "d")
+# screen.onkey(clear, "c")
+# screen.listen()
 
-def backwards():
-    t.backward(10)
+t1 = Turtle(shape="turtle")
+t1.color(colors[0])
+t1.penup()
+t1.goto(x=-230,y=-100)
 
-def clockwise():
-    t.right(10)
+t2 = Turtle(shape="turtle")
+t2.color(colors[1])
+t2.penup()
+t2.goto(x=-230,y=-60)
 
-def counter_clockwise():
-    t.left(10)
+t3 = Turtle(shape="turtle")
+t3.color(colors[2])
+t3.penup()
+t3.goto(x=-230,y=-20)
 
-def clear():
-    t.clear()
+t4 = Turtle(shape="turtle")
+t4.color(colors[3])
+t4.penup()
+t4.goto(x=-230,y=20)
 
-screen.onkey(forwards, "w")
-screen.onkey(backwards, "s")
-screen.onkey(counter_clockwise, "a")
-screen.onkey(clockwise, "d")
-screen.onkey(clear, "c")
+t5 = Turtle(shape="turtle")
+t5.color(colors[4])
+t5.penup()
+t5.goto(x=-230,y=60)
+
+t6 = Turtle(shape="turtle")
+t6.color(colors[5])
+t6.penup()
+t6.goto(x=-230,y=100)
 
 
-screen.listen()
+
 
 screen.exitonclick()
