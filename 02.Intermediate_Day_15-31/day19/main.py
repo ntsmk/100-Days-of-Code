8844c9bb52e4155b2fa0a3fb138b522d378040d7
ntsmk <1991.ntm@gmail.com>
2024-09-11 11:36:22 -0700

    finished it

--- 02.Intermediate_Day_15-31/day19/main.py
+++ 02.Intermediate_Day_15-31/day19/main.py
@@ -9,11 +9,19 @@ def forwards():
 def backwards():
     t.backward(10)
 
+def clockwise():
+    t.right(10)
+
+def counter_clockwise():
+    t.left(10)
+
 def clear():
     t.clear()
 
 screen.onkey(forwards, "w")
 screen.onkey(backwards, "s")
+screen.onkey(counter_clockwise, "a")
+screen.onkey(clockwise, "d")
 screen.onkey(clear, "c")
 
 
