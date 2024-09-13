d2c60c1c2843371bb5430ad6eaeb415b428f8a36
ntsmk <1991.ntm@gmail.com>
2024-09-13 10:57:52 -0700

    editing

--- 02.Intermediate_Day_15-31/day20/snake.py
+++ 02.Intermediate_Day_15-31/day20/snake.py
@@ -1,11 +1,18 @@
 from turtle import Screen, Turtle
 import time
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
 t_list = ["t1", "t2", "t3"]
 
 class Snake():
     def __init__(self):
         self.create_snake()
+        self.head = t_list[0]
 
     def create_snake(self):
         for t_index in range(3):
@@ -16,21 +23,21 @@ class Snake():
 
     def move(self):
         for i in range(len(t_list) - 1, 0, -1):
-            new_x = t_list[i -1].xcor()
+            new_x = t_list[i - 1].xcor()
             new_y = t_list[i - 1].ycor()
-            t_list[i].goto(new_x,new_y)
-        t_list[0].forward(20)
+            t_list[i].goto(new_x, new_y)
+        self.head.forward(20)
 
     def up(self):
-        t_list[0].setheading(90)
+        self.head.setheading(UP)
 
 
     def down(self):
-        t_list[0].setheading(270)
+        self.head.setheading(DOWN)
 
     def left(self):
-        t_list[0].setheading(180)
+        self.head.setheading(LEFT)
 
 
     def right(self):
-        t_list[0].setheading(0)
+        self.head.setheading(RIGHT)
