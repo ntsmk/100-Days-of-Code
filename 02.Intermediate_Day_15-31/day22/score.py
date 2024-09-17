d56c7adf4bd8741a8d6a475d7cae05f7db4ab649
Nat <1991.ntm@gmail.com>
2024-09-16 22:47:48 -0700

    cleaningup

--- 02.Intermediate_Day_15-31/day22/score.py
+++ 02.Intermediate_Day_15-31/day22/score.py
@@ -1,16 +1,34 @@
 from turtle import Turtle
 
+ALIGN = "center"
+FONT = ('Courier New', 40, 'normal')
+
 class Score(Turtle):
     def __init__(self):
         super().__init__()
+        self.color("white")
+        self.hideturtle()
+        self.penup()
         self.score_r = 0
         self.score_l = 0
+        self.update_r()
+        self.update_l()
+
+    def update_r(self):
+        self.goto(200,230)
+        self.write(self.score_r, False, ALIGN, FONT)
+
+    def update_l(self):
+        self.goto(-200, 230)
+        self.write(self.score_l, False, ALIGN, FONT)
 
     def add_score_r(self):
         self.score_r += 1
-        #print(f"right score: {self.score_r}")
+        self.clear()
+        self.update_r()
 
     def add_score_l(self):
         self.score_l += 1
-        #print(f"left score: {self.score_l}")
+        self.clear()
+        self.update_l()
 
