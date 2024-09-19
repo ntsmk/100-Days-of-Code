4e2e5f857d49d9e329ad089ae23978f92cc562b2
Nat <1991.ntm@gmail.com>
2024-09-18 22:40:13 -0700

    adding car class

--- 02.Intermediate_Day_15-31/day23/car.py
+++ 02.Intermediate_Day_15-31/day23/car.py
@@ -6,20 +6,23 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 cars = []
 
-class Car(Turtle):
+class Car():
     def __init__(self):
-        for i in range(5):
-            super().__init__()
-            self.shape("square")
-            self.penup()
-            self.shapesize(stretch_len=2, stretch_wid=1)
-            self.goto(300, random.randint(-260, 260))
-            self.setheading(180)
-            self.color(random.choice(COLOR))
+        self.generate()
 
-    def generate_car(self):
-        pass
-        
+
+    def generate(self):
+        for car in range(50):
+            car = Turtle(shape="turtle")
+            car.shape("square")
+            car.penup()
+            car.shapesize(stretch_len=2, stretch_wid=1)
+            car.goto(random.randint(-300, 300), random.randint(-260, 260))
+            car.setheading(180)
+            car.color(random.choice(COLOR))
+            cars.append(car)
+
     def move(self):
-        self.forward(STARTING_MOVE_DISTANCE)
+        for car in cars:
+            car.forward(STARTING_MOVE_DISTANCE)
 
