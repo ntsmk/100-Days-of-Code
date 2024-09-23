830b2b256fa466f35f6576fb6a1032c14eeeb392
ntsmk <1991.ntm@gmail.com>
2024-09-23 11:59:11 -0700

    completed challenge

--- 02.Intermediate_Day_15-31/day24/mail_challenge/Mail Merge Project Start/main.py
+++ 02.Intermediate_Day_15-31/day24/mail_challenge/Mail Merge Project Start/main.py
@@ -7,7 +7,17 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+text_list = []
+old_text = open(r"Input\Letters\starting_letter.txt", "r")
+text = ''.join(old_text.readlines())
+names = open(r"Input\Names\invited_names.txt", "r")
+names_text = names.readlines()
 
-f = open(r"Input\Letters\starting_letter.txt", "r")
-print(f.readlines())
+for i in range(len(names_text)):
+    new_text = text.replace("[name]",names_text[i])
+    text_list.append(new_text)
+
+for i in range(len(names_text)):
+    with open(f"Output/ReadyToSend/letter{i}.txt", mode="w") as file:
+        file.write(text_list[i])
 
