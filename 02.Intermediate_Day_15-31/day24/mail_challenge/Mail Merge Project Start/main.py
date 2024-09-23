82d0aefce8594847bbc8b4e467eef2e1d64ab1f0
ntsmk <1991.ntm@gmail.com>
2024-09-23 12:09:21 -0700

    adding comments

--- 02.Intermediate_Day_15-31/day24/mail_challenge/Mail Merge Project Start/main.py
+++ 02.Intermediate_Day_15-31/day24/mail_challenge/Mail Merge Project Start/main.py
@@ -7,13 +7,13 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-old_text = open(r"Input\Letters\starting_letter.txt", "r")
-text = ''.join(old_text.readlines())
-names = open(r"Input\Names\invited_names.txt", "r")
-names_text = names.readlines()
+with open(r"Input\Letters\starting_letter.txt", "r") as old_text: # open starting letter as read mode
+    text = ''.join(old_text.readlines()) # converting list into str
+with open(r"Input\Names\invited_names.txt", "r") as names: # open name list as read mode
+    names_text = names.readlines() # this is already list
 
 for i in range(len(names_text)):
-    new_text = text.replace("[name]",names_text[i])
-    with open(f"Output/ReadyToSend/letter{i}.txt", mode="w") as file:
-        file.write(new_text)
+    new_text = text.replace("[name]",names_text[i]) # replace each names with [name] blank
+    with open(f"Output/ReadyToSend/letter{i}.txt", mode="w") as file: # creates each letters txt file
+        file.write(new_text) # writing new text already replaced
 
