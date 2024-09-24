3a4b692df21afa67b084f7d137014aa77272142d
ntsmk <1991.ntm@gmail.com>
2024-09-24 12:07:13 -0700

    adding pasndas

--- 02.Intermediate_Day_15-31/day25/main.py
+++ 02.Intermediate_Day_15-31/day25/main.py
@@ -1,4 +1,25 @@
-with open("weather_data.csv") as f:
-    data = f.readlines()
+# with open("weather_data.csv") as f:
+#     data = f.readlines()
+#
+# print(data)
 
-print(data)
+# import csv
+#
+# with open("weather_data.csv") as f:
+#     data = csv.reader(f)
+#     temperatures = []
+#
+#     for row in data:
+#         temperatures.append(row[1])
+#     temperatures.remove("temp")
+#     for i in range(len(temperatures)):
+#         temperatures[i] = int(temperatures[i])
+#
+# print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+
+data_dict = data.to_dict()
+print(data_dict)
