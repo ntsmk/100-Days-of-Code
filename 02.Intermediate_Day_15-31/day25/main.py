9aa7196dc6408c5c1378ec6c102191f079a5894d
ntsmk <1991.ntm@gmail.com>
2024-09-24 15:36:26 -0700

    adding dataframe

--- 02.Intermediate_Day_15-31/day25/main.py
+++ 02.Intermediate_Day_15-31/day25/main.py
@@ -18,8 +18,33 @@
 # print(temperatures)
 
 import pandas
+from statistics import mean
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 
-data_dict = data.to_dict()
-print(data_dict)
+# # data_dict = data.to_dict()
+# # print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# # average = mean(temp_list)
+# # print(round(average,0))
+# # print(data["temp"].mean())
+# # print(data["temp"].max())
+#
+# print(data.temp)
+
+# print(data[data.temp == data.temp.max()])
+#
+# monday = data[data.day == 'Monday']
+# celsius = monday.temp
+# fahrenheit = (celsius * 1.8) + 32
+# print(f"celsius: {celsius}")
+# print(f"fahrenheit: {fahrenheit}")
+
+data_dic ={
+    "student": ["Amy", "james", "angela"],
+    "scores": [78, 57,29]
+}
+data = pandas.DataFrame(data_dic)
+print(data)
+data.to_csv("newdata.csv")
