34c5451e64d1b8a886829ccd9537b55c5c5c3a11
ntsmk <1991.ntm@gmail.com>
2024-09-26 13:24:38 -0700

    adding more

--- 02.Intermediate_Day_15-31/day25/day-25-us-states-game-start/main.py
+++ 02.Intermediate_Day_15-31/day25/day-25-us-states-game-start/main.py
@@ -11,26 +11,36 @@ turtle.shape(img)
 
 
 
-answer_state = screen.textinput(title=input_title, prompt="What's another state?").title()
+
 
 # todo read CSV file 50_states.csv
 data = pandas.read_csv("50_states.csv")
 state_list = data["state"].to_list()
+correct_list = []
 
 # todo take input and see if it matches among the 50 states list
 isCorrect = False
-for state in state_list:
-    if state == answer_state:
-        isCorrect = True
-    else:
-        isCorrect = False
+isGameon = True
+while isGameon:
+    answer_state = screen.textinput(title=input_title, prompt="What's another state?").title()
+    for state in state_list:
+        if state == answer_state:
+            isCorrect = True
+        else:
+            isCorrect = False
 
-print(isCorrect)
+    # print(isCorrect)
 
-# todo if matches, the word goes to x and y. increase the score (states)
-correct_state = data[data.state == answer_state]
-# print(int(correct_state.x), int(correct_state.y))
+    # todo if matches, the word goes to x and y. increase the score (states)
+    correct_state = data[data.state == answer_state]
+    # print(int(correct_state.x), int(correct_state.y))
 
-# if not matches, nothing happens,
+    # if not matches, nothing happens,
 
-# todo count the score xx /50 states correct and show it on the title
+    # todo count the score xx /50 states correct and show it on the title
+    correct_list.append(str(correct_state.state))
+    if answer_state not in state_list:
+        isGameon = False
+
+print(correct_list)
+
