10893b8b8a8d337aca2864f19d63c9940e2d5545
Nat <1991.ntm@gmail.com>
2024-09-29 14:05:41 -0700

    adding exercise 2

--- 02.Intermediate_Day_15-31/day26/main.py
+++ 02.Intermediate_Day_15-31/day26/main.py
@@ -1,3 +1,4 @@
+# Dictionary Comprehension 1
 sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 # print(sentence.split())
 # for item in sentence.split():
@@ -5,3 +6,10 @@ sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
 
 result = {key:len(key) for key in sentence.split()}
 print(result)
+
+# Dictionary Comprehension 2
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+
+weather_f = {key:(value* 9/5) + 32 for key,value in weather_c.items()}
+
+print(weather_f)
