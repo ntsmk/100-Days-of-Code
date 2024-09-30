9a58f258879f29c8cc30110ac3b21eccf25edf89
Nat <1991.ntm@gmail.com>
2024-09-30 00:01:42 -0700

    finished todo 2

--- 02.Intermediate_Day_15-31/day26/NATO-alphabet-start/NATO-alphabet-start/main.py
+++ 02.Intermediate_Day_15-31/day26/NATO-alphabet-start/NATO-alphabet-start/main.py
@@ -33,4 +33,12 @@ print(new_dict)
 #     print(f"{row["letter"]}:{row["code"]}")
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+user_input = input("Enter a word: ")
+input_list = [letter.upper() for letter in user_input]
+print(input_list)
+# answer_list = [word for word in new_dict.items() if ]
+# print(answer_list)
+thislist = []
+for i in input_list:
+    thislist.append(new_dict[i])
+print(thislist)
