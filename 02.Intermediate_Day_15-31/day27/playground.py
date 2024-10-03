35e3a9653cbe4ab03a284ac3b00582fecc113e9b
ntsmk <1991.ntm@gmail.com>
2024-10-03 10:51:57 -0700

    working on **kw

--- 02.Intermediate_Day_15-31/day27/playground.py
+++ 02.Intermediate_Day_15-31/day27/playground.py
@@ -7,3 +7,17 @@ def add(*args): # you can actually use anything after *, like *numbers works too
 a = add(13,10,12,14)
 print(a)
 
+def calculate(**kwargs): # kwargs = keyword argument
+    print(type(kwargs))
+    print(kwargs["add"])
+
+calculate(add=3, multiply=5)
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw["make"]
+        self.model = kw["model"]
+
+my_car = Car(make="Toyota", model="RAV4")
+
+print(my_car.make)
