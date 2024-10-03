1fb24797caf44589129b2e62637f1acbe5ae719d
ntsmk <1991.ntm@gmail.com>
2024-10-03 16:55:05 -0700

    editing more

--- 02.Intermediate_Day_15-31/day27/main.py
+++ 02.Intermediate_Day_15-31/day27/main.py
@@ -1,3 +1,4 @@
+#from tkinter import *
 import tkinter
 
 window = tkinter.Tk()
@@ -5,6 +6,18 @@ window.title("welcome to my program")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack(side="bottom")
+my_label.pack()
+
+def button_clicked():
+    my_label["text"] = "It got clicked"
+
+button = tkinter.Button(text="push!", command=button_clicked)
+button.pack()
+
+input = tkinter.Entry(width=10)
+input.pack()
+print(input.get())
+
+
 
 window.mainloop()
--- 02.Intermediate_Day_15-31/day27/playground.py
+++ 02.Intermediate_Day_15-31/day27/playground.py
@@ -21,3 +21,17 @@ class Car:
 my_car = Car(make="Toyota")
 
 print(my_car.model)
+
+
+def test(*args):
+    print(args)
+
+
+test(1, 2, 3, 5)
+
+
+def all_aboard(a, *args, **kw):
+    print(a, args, kw)
+
+
+all_aboard(4, 7, 3, 0, x=10, y=64)
