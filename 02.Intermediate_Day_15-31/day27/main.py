671b71480a9125bbe0eeee89bf1879c76c2c386c
Nat <1991.ntm@gmail.com>
2024-10-05 15:57:46 -0700

    finishing exercise

--- 02.Intermediate_Day_15-31/day27/main.py
+++ 02.Intermediate_Day_15-31/day27/main.py
@@ -5,22 +5,29 @@ window = tkinter.Tk()
 window.title("welcome to my program")
 window.minsize(width=500, height=300)
 
+
+# label
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
 # my_label.pack()
 # my_label.place(x=100,y=0)
 my_label.grid(column=0, row=0)
 
+# button
 def button_clicked():
     my_label["text"] = input.get()
 
-button = tkinter.Button(text="push!", command=button_clicked)
+button1 = tkinter.Button(text="push!", command=button_clicked)
 # button.pack()
-button.grid(column=1, row=1)
+button1.grid(column=1, row=1)
 
+button2 = tkinter.Button(text="push!", command=button_clicked)
+button2.grid(column=2, row=0)
+
+# entry
 input = tkinter.Entry(width=10)
 # input.pack()
 # print(input.get())
-input.grid(column=2, row=2)
+input.grid(column=3, row=2)
 
 
 
