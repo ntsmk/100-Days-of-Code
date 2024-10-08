16b6ac0c4aedb25dc9f5539ac01000e4a3c6add9
ntsmk <1991.ntm@gmail.com>
2024-10-08 11:20:03 -0700

    added todo

--- 02.Intermediate_Day_15-31/day28/pomodoro-start/main.py
+++ 02.Intermediate_Day_15-31/day28/pomodoro-start/main.py
@@ -19,10 +19,23 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=50, pady=50, bg=YELLOW)
+
 img_file = PhotoImage(file="tomato.png")
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=img_file)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
+
+# todo 1 put big "Timer"
+timer = Label(text="Timer")
+# timer.grid(column=1, row=0)
+# todo 2 put start and reset button
+
+# todo 3 put check marks
+
+# hint: use grid instead
+# hint: copy and paste the check mark from web
+
 
 window.mainloop()
