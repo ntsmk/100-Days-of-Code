ad861248c43896c2fb477670622ba021b923cafd
ntsmk <1991.ntm@gmail.com>
2024-10-08 17:57:08 -0700

    adding count_down

--- 02.Intermediate_Day_15-31/day28/pomodoro-start/main.py
+++ 02.Intermediate_Day_15-31/day28/pomodoro-start/main.py
@@ -14,31 +14,46 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    # print(count)
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count-1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=50, pady=50, bg=YELLOW)
 
+
+# def say_something(a,b,c):
+#     print(a)
+#     print(b)
+#     print(c)
+# window.after(3000, say_something, 3,5,8)
+
 img_file = PhotoImage(file="tomato.png")
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=img_file)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 # canvas.pack()
 canvas.grid(column=1, row=1)
 
-# todo 1 put big "Timer"
+count_down(5)
+
+# 1 put big "Timer"
 timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30, "bold"))
 timer.grid(column=1, row=0)
 
-# todo 2 put start and reset button
-start_button = Button(text="Start")
+# 2 put start and reset button
+start_button = Button(text="Start", highlightthickness=0)
 start_button.grid(column=0, row=2)
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
-# todo 3 put check marks
+# 3 put check marks
 checkmark = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15))
 checkmark.grid(column=1, row=3)
 
