badbec69f4c51645d1c59cdef223b20fc3edd739
ntsmk <1991.ntm@gmail.com>
2024-10-10 16:53:42 -0700

    adding reps

--- 02.Intermediate_Day_15-31/day28/pomodoro-start/main.py
+++ 02.Intermediate_Day_15-31/day28/pomodoro-start/main.py
@@ -10,12 +10,20 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    # todo figure it out how to set "reps"
+
+
     count_down(5*60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
