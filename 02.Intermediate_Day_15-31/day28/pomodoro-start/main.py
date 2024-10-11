d724fad9241c1e489fa9c5a319233561ee71691b
ntsmk <1991.ntm@gmail.com>
2024-10-11 14:25:46 -0700

    prob finished challenge

--- 02.Intermediate_Day_15-31/day28/pomodoro-start/main.py
+++ 02.Intermediate_Day_15-31/day28/pomodoro-start/main.py
@@ -18,17 +18,27 @@ reps = 0
 
 def start_timer():
     global reps
+    reps += 1
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
-    # todo figure it out how to set "reps"
+    # todo figure it out how to set "reps" -> prob finished
+
+    # when rep = 1,3,5,7:
+    if reps % 2 != 0:
+        count_down(work_sec)
+
+    # when rep = 2,4,6
+    elif reps == 8:
+        count_down(long_break_sec)
+
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
 
 
-    count_down(5*60)
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-
+    global reps
     count_min = math.floor(count/60)
     count_sec = count % 60
     if count_sec == 0:
@@ -41,25 +51,20 @@ def count_down(count):
     if count > 0:
         window.after(1000, count_down, count-1)
 
+    if count_min == 0 and count_sec == "00":
+        reps += 1
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=50, pady=50, bg=YELLOW)
 
-
-# def say_something(a,b,c):
-#     print(a)
-#     print(b)
-#     print(c)
-# window.after(3000, say_something, 3,5,8)
-
 img_file = PhotoImage(file="tomato.png")
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image=img_file)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-# canvas.pack()
 canvas.grid(column=1, row=1)
 
 # count_down(5)
@@ -77,10 +82,6 @@ reset_button.grid(column=2, row=2)
 # 3 put check marks
 checkmark = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15))
 checkmark.grid(column=1, row=3)
-
-# hint: use grid instead
-# hint: copy and paste the check mark from web "✔"
-# hint: use "fg=GREEN" fpr the foreground
 
 
 window.mainloop()
