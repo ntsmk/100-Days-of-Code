b032b39b43f0006abcd1aaeac67b6468c10a60d4
ntsmk <1991.ntm@gmail.com>
2024-10-15 14:37:53 -0700

    adding the pic

--- 02.Intermediate_Day_15-31/day29/password-manager-start/main.py
+++ 02.Intermediate_Day_15-31/day29/password-manager-start/main.py
@@ -1,6 +1,20 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
 # todo set up the logo image as it is shown in the video.
+window = Tk()
+window.title("Password manager")
+
+img_file = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200)
+window.config(padx=20, pady=20)
+
+canvas.create_image(100, 100, image=img_file)
+canvas.grid(column=0, row=0)
+
+
+window.mainloop()
