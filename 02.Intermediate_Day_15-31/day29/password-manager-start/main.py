757043d3ba3bd609ccc32439b990522141f75e31
ntsmk <1991.ntm@gmail.com>
2024-10-15 15:27:11 -0700

    finishing grid layout

--- 02.Intermediate_Day_15-31/day29/password-manager-start/main.py
+++ 02.Intermediate_Day_15-31/day29/password-manager-start/main.py
@@ -5,7 +5,7 @@ from tkinter import *
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
-# todo set up the logo image as it is shown in the video.
+
 window = Tk()
 window.title("Password manager")
 
@@ -14,7 +14,33 @@ canvas = Canvas(width=200, height=200)
 window.config(padx=20, pady=20)
 
 canvas.create_image(100, 100, image=img_file)
-canvas.grid(column=0, row=0)
+canvas.grid(column=1, row=0)
+
+# todo setting up other user interface
+# hint use columspan=xx parameter in grid
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+pass_label = Label(text="Password:")
+pass_label.grid(column=0, row=3)
+
+generate_button = Button(text="Generate Password")
+generate_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
+
+web_input = Entry(width=35)
+web_input.grid(column=1, row=1, columnspan=2)
+
+email_input = Entry(width=35)
+email_input.grid(column=1, row=2, columnspan=2)
+
+pass_input = Entry(width=21)
+pass_input.grid(column=1, row=3)
 
 
 window.mainloop()
