663873ea545f98ab3623fb3e07a2a790b786aee6
Nat <1991.ntm@gmail.com>
2024-10-20 12:20:35 -0700

    adding raise

--- 02.Intermediate_Day_15-31/day30/main.py
+++ 02.Intermediate_Day_15-31/day30/main.py
@@ -1,12 +1,29 @@
-# with open("a_file.txt") as file:
-#     file.read()
+# # with open("a_file.txt") as file:
+# #     file.read()
+#
+# try:
+#     file = open("a_file.txt")
+#     a_dic = {"key": "value"}
+#     print(a_dic["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("something")
+# except KeyError as error_message:
+#     print(f"the key {error_message} does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     # file.close()
+#     # print("file was closed")
+#     raise TypeError("This is an error that I made up")
 
-try:
-    file = open("a_file.txt")
-    a_dic = {"key": "value"}
-    print(a_dic["abc"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("something")
-except KeyError as error_message:
-    print(f"the key {error_message} does not exist")
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters")
+
+bmi = weight / height ** 2
+print(bmi)
