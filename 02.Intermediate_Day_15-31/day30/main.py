246306de7f000941d941464fde56c1fc2d5c02f1
Nat <1991.ntm@gmail.com>
2024-10-20 13:08:50 -0700

    adding coding exercise

--- 02.Intermediate_Day_15-31/day30/main.py
+++ 02.Intermediate_Day_15-31/day30/main.py
@@ -29,6 +29,7 @@
 # print(bmi)
 #
 #
+# coding exercise 1
 # fruits = ["Apple", "Pear", "Orange"]
 #
 # # Catch the exception and make sure the code runs without crashing.
@@ -57,3 +58,28 @@ def make_pie(index):
 make_pie(4)
 
 # looks it is normal to put try and except in the actual method
+
+# coding exercise 2
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+
+def count_likes(posts):
+    total_likes = 0
+    for post in posts:
+        try:
+            total_likes = total_likes + post['Likes']
+        except KeyError:
+            total_likes += 0
+
+    return total_likes
+
+
+count_likes(facebook_posts)
+
