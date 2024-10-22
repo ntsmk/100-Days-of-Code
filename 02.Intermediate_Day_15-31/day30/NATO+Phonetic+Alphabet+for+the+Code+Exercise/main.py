6476896f796252caeda884c012a18300ccae1782
ntsmk <1991.ntm@gmail.com>
2024-10-21 17:13:07 -0700

    adding but not completed

--- 02.Intermediate_Day_15-31/day30/NATO+Phonetic+Alphabet+for+the+Code+Exercise/main.py
+++ 02.Intermediate_Day_15-31/day30/NATO+Phonetic+Alphabet+for+the+Code+Exercise/main.py
@@ -2,16 +2,16 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-# todo put exception to catch anything other than alphabet and tell warning.
-# KeyError
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-
-word = input("Enter a word: ").upper()
+# todo put exception to catch anything other than alphabet and tell warning.
+# KeyError
+# it ask but not keep asking. should I write while loop?
 try:
+    word = input("Enter a word: ").upper()
     output_list = [phonetic_dict[letter] for letter in word]
 except KeyError:
     print("Sorry, only letters in the alphabet please.")
-    word = input("Enter a word: ").upper()
-print(output_list)
+else:
+    print(output_list)
