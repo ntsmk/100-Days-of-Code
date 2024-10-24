5dcd2e8ff80d32ee6f14a84f7a71fa35092ed214
Nat <1991.ntm@gmail.com>
2024-10-24 14:54:32 -0700

    in progress of UI part

--- 02.Intermediate_Day_15-31/day31/flash-card-project-start/main.py
+++ 02.Intermediate_Day_15-31/day31/flash-card-project-start/main.py
@@ -3,23 +3,25 @@ from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
 
 # ---------------------------- UI SETUP ------------------------------- #
-# todo Step 1 - Create the User Interface (UI) with Tkinter
-# only 2 X 2 grid. use card_front.png
 
 window = Tk()
 window.title("Flash card app")
 
+# todo need to put label French and word
 # canvas
-canvas = Canvas(width=800, height=526)
-window.config(padx=50, pady=50)
-canvas.grid(column=0, row=0)
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+front = PhotoImage(file="images/card_front.png")
+canvas.create_image(401,265,image=front)
+canvas.grid(column=0, row=0, columnspan=2)
 
 # button
 right = PhotoImage(file="images/right.png")
 right_button = Button(image=right, highlightthickness=0)
-right_button.grid(column=1, row=0)
+right_button.grid(column=1, row=1)
+
 left = PhotoImage(file="images/wrong.png")
 wrong_button = Button(image=left, highlightthickness=0)
-wrong_button.grid(column=0, row=0)
+wrong_button.grid(column=0, row=1)
 
 window.mainloop()
