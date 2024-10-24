99db938ed919992a390e42537043159914795e81
Nat <1991.ntm@gmail.com>
2024-10-24 16:29:06 -0700

    half size

--- 02.Intermediate_Day_15-31/day31/flash-card-project-start/main.py
+++ 02.Intermediate_Day_15-31/day31/flash-card-project-start/main.py
@@ -12,7 +12,7 @@ window.title("Flash card app")
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 front = PhotoImage(file="images/card_front.png")
-canvas.create_image(401,265,image=front)
+canvas.create_image(400,263,image=front)
 canvas.grid(column=0, row=0, columnspan=2)
 
 # button
