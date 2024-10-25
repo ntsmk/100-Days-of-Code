b1d14aabd4fe2ac9d738280e024a46e58efff834
ntsmk <1991.ntm@gmail.com>
2024-10-25 16:16:47 -0700

    editting flip func

--- 02.Intermediate_Day_15-31/day31/flash-card-project-start/main.py
+++ 02.Intermediate_Day_15-31/day31/flash-card-project-start/main.py
@@ -3,27 +3,23 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-# ---------------------------- NEW FLASH CARDS ------------------------------- #
 df = pandas.read_csv("data/french_words.csv")
 dict = df.to_dict(orient="records")
 
+# ---------------------------- NEW FLASH CARDS ------------------------------- #
 def generate():
     thisdict = random.choice(dict)
-    word.config(text=thisdict["French"])
-    language.config(text=list(thisdict.keys())[0])
+    canvas.itemconfig(word, text=thisdict["French"])
+    canvas.itemconfig(language,text=list(thisdict.keys())[0])
 
-
-# todo Step 3 - Flip the Cards
-# IMPORTANT: PhotoImage objects should not be created inside a function. Otherwise, it will not work.
-back = PhotoImage(file="images/card_back.png")
 def flip():
+    # todo pick the English translation, not random word
     # todo figure out how to count 3 seconds
+    # timer = window.after(1000, count_down, count-1) how to modify this
     thisdict = random.choice(dict)
-    word.config(text=thisdict["English"])
-    language.config(text=list(thisdict.keys())[1])
-    canvas.create_image(400,263,image=back)
-    canvas.grid(column=0, row=0, columnspan=2)
+    canvas.itemconfig(word, text=thisdict["English"], fill="White")
+    canvas.itemconfig(language,text=list(thisdict.keys())[1], fill="White")
+    canvas.itemconfig(canvas_image, image=back)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -34,19 +30,17 @@ window.title("Flash card app")
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 front = PhotoImage(file="images/card_front.png")
-canvas.create_image(400,263,image=front)
+back = PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(400,263,image=front)
 canvas.grid(column=0, row=0, columnspan=2)
 
 # label
-language = Label(text="", font=("Arial", 40, "italic"), bg="white")
-language.place(x=300, y=110)
-
-word = Label(text="", font=("Arial", 60, "bold"),  bg="white")
-word.place(x=270, y=213)
+language = canvas.create_text(370, 120, text="", font=("Arial", 40, "italic"))
+word = canvas.create_text(370, 230, text="", font=("Arial", 60, "bold"))
 
 # button
 right = PhotoImage(file="images/right.png")
-right_button = Button(image=right, highlightthickness=0, command=generate)
+right_button = Button(image=right, highlightthickness=0, command=flip)
 right_button.grid(column=1, row=1)
 
 left = PhotoImage(file="images/wrong.png")
@@ -54,6 +48,6 @@ wrong_button = Button(image=left, highlightthickness=0, command=generate)
 wrong_button.grid(column=0, row=1)
 
 generate()
-flip()
+# flip()
 
 window.mainloop()
