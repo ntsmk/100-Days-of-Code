a311224ea7d63a47e6eef6c17ce13d798c3b4c1f
ntsmk <1991.ntm@gmail.com>
2024-10-30 14:48:49 -0700

    moving to datetime

--- 03.Intermediate+_Day_32-58/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ 03.Intermediate+_Day_32-58/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -1,3 +1,13 @@
-import smtplib
+# import smtplib
+#
+# my_email = "whereismycoffee9@gmail.com"
+# password = ""
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs=my_email,
+#                         msg="Subject:We wish you Merry Christmas\n\nThis is body of my email")
 
-connection = smtplib.SMTP()
+import datetime
