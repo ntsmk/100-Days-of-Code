7849873657dd56eb71954c3494eb01165a30c74b
ntsmk <1991.ntm@gmail.com>
2024-10-30 16:28:29 -0700

    adding

--- 03.Intermediate+_Day_32-58/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ 03.Intermediate+_Day_32-58/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -10,4 +10,13 @@
 #                         to_addrs=my_email,
 #                         msg="Subject:We wish you Merry Christmas\n\nThis is body of my email")
 
-import datetime
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+print(year, month)
+
+date_of_birth = dt.datetime(year=1996, month=3, day=8)
+
+print(date_of_birth)
