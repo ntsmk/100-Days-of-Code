cc091d22b263a55f074613c552d94ac9d21c6dc7
ntsmk <1991.ntm@gmail.com>
2024-10-31 11:44:22 -0700

    adding the small project

--- 03.Intermediate+_Day_32-58/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ 03.Intermediate+_Day_32-58/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -1,22 +1,32 @@
-# import smtplib
-#
-# my_email = "whereismycoffee9@gmail.com"
-# password = ""
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email,
-#                         to_addrs=my_email,
-#                         msg="Subject:We wish you Merry Christmas\n\nThis is body of my email")
-
+import smtplib
+import random
 import datetime as dt
+import schedule
+
+with open("quotes.txt") as file:
+    random_quote = random.choice(file.readlines())
+
+my_email = "whereismycoffee9@gmail.com"
+password = ""
+
+def send_email():
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=my_email,
+                            msg=f"Subject:Here is inspirational quote for you\n\n{random_quote}")
 
 now = dt.datetime.now()
 year = now.year
 month = now.month
-print(year, month)
-
+day_of_week = now.weekday()
+# print(day_of_week) Thursday = 3
+# print(year, month)
 date_of_birth = dt.datetime(year=1996, month=3, day=8)
+# print(date_of_birth)
 
-print(date_of_birth)
+if day_of_week == 2:
+    send_email()
+else:
+    print("today is not that day")
