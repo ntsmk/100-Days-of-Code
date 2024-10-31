4d560a2958f5c0dba5d6fab89563a44c2061459f
ntsmk <1991.ntm@gmail.com>
2024-10-31 16:54:20 -0700

    figured if it matches or not

--- 03.Intermediate+_Day_32-58/day32/birthday-wisher-extrahard-start/main.py
+++ 03.Intermediate+_Day_32-58/day32/birthday-wisher-extrahard-start/main.py
@@ -1,13 +1,17 @@
 import smtplib
 import datetime as dt
+import pandas
 
-# 2. Check if today matches a birthday in the birthdays.csv
 now = dt.datetime.now()
-month = now.month
-day = now.day
-print(month,day)
+current_month = now.month
+current_day = now.day
 
-with open("birthdays.csv") as file
+df = pandas.read_csv("birthdays.csv")
+dict = df.to_dict(orient="records")
+for i in range(len(dict)):
+    if dict[i]['month'] == current_month and dict[i]['day'] == current_day:
+        print(dict[i]['name'])
+
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
