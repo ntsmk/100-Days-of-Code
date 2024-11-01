79a15b790530aece6f180297b43e5dfe5b4644cd
ntsmk <1991.ntm@gmail.com>
2024-11-01 09:58:11 -0700

    finished

--- 03.Intermediate+_Day_32-58/day32/birthday-wisher-extrahard-start/main.py
+++ 03.Intermediate+_Day_32-58/day32/birthday-wisher-extrahard-start/main.py
@@ -9,25 +9,12 @@ current_day = now.day
 
 df = pandas.read_csv("birthdays.csv")
 dict = df.to_dict(orient="records")
-for i in range(len(dict)):
-    if dict[i]['month'] == current_month and dict[i]['day'] == current_day:
-        the_name = dict[i]['name']
-        the_email = dict[i]['email']
-        print(the_name)
-        print(the_email)
-
-random_number = random.randint(1,3)
-with open(f"letter_templates/letter_{random_number}.txt") as file:
-    f = file.read()
-    try:
-        replaced_text = f.replace("[NAME]", the_name)
-        print(replaced_text)
-    except NameError:
-        print("the birthday person not found")
-
 
 my_email = "whereismycoffee9@gmail.com"
 password = ""
+
+replaced_text = ""
+
 def send_email():
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
@@ -35,6 +22,16 @@ def send_email():
         connection.sendmail(from_addr=my_email,
                             to_addrs=the_email,
                             msg=f"Subject:Happy Birthday\n\n{replaced_text}")
+    print("mail sent")
 
-if the_name != "":
-    send_email()
+for i in range(len(dict)):
+    if dict[i]['month'] == current_month and dict[i]['day'] == current_day:
+        the_name = dict[i]['name']
+        the_email = dict[i]['email']
+        random_number = random.randint(1, 3)
+        with open(f"letter_templates/letter_{random_number}.txt") as file:
+            f = file.read()
+            replaced_text = f.replace("[NAME]", the_name)
+        send_email()
+    else:
+        print("this is not the birthday person")
