3bff1ad86834d1ff0d18783008ba1bfd54336ee7
ntsmk <1991.ntm@gmail.com>
2024-11-07 09:03:16 -0700

    adding more

--- 03.Intermediate+_Day_32-58/day33/main.py
+++ 03.Intermediate+_Day_32-58/day33/main.py
@@ -1,13 +1,27 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
+MY_LAT = 60.719560486554776
+MY_LNG = -135.05267151196642
 
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# latitude = data['iss_position']['latitude']
+# longitude = data['iss_position']['longitude']
+#
+# position = (latitude,longitude)
+#
+# print(position)
+# 60.719560486554776, -135.05267151196642
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
 data = response.json()
 
-latitude = data['iss_position']['latitude']
-longitude = data['iss_position']['longitude']
-
-position = (latitude,longitude)
-
-print(position)
+print(data)
