b1d84c839a8e397377c77f61a6bfdc4efdca1a9e
ntsmk <1991.ntm@gmail.com>
2024-11-08 17:12:42 -0700

    adding more

--- 03.Intermediate+_Day_32-58/day33/main.py
+++ 03.Intermediate+_Day_32-58/day33/main.py
@@ -19,17 +19,18 @@ MY_LNG = -135.05267151196642
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LNG,
+    "formatted": 0,
 }
 
 response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"]
-sunset = data["results"]["sunset"]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
 
 print(sunrise)
 print(sunset)
 
 time_now = datetime.now()
-print(time_now)
+print(time_now.hour)
