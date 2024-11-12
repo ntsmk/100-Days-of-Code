0785592ac5112e8a30fc061b829851c7e9a13730
Nat <1991.ntm@gmail.com>
2024-11-12 15:17:33 -0700

    prob finishing challenge

--- 03.Intermediate+_Day_32-58/day33/issoverhead-start/main.py
+++ 03.Intermediate+_Day_32-58/day33/issoverhead-start/main.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 import smtplib
+import time
 
 my_email = "whereismycoffee9@gmail.com"
 password = ""
@@ -8,7 +9,7 @@ password = ""
 MY_LAT = 60.721188 # Whitehorse latitude
 MY_LONG = -135.056839 # Whitehorse longitude
 
-# API 1: ISS location
+# API 1: getting ISS location
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 data = response.json()
@@ -16,14 +17,13 @@ data = response.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
+# API 2 getting sunrise/sunset time
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LONG,
     "tzid": "America/Whitehorse",
     "formatted": 0,
 }
-
-# API 2 sunrise/sunset time
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
@@ -42,16 +42,16 @@ print(sunrise)
 print(sunset)
 print(time_now_hour)
 
-if iss_latitude -5 <= MY_LAT <= iss_latitude +5 and iss_longitude -5 <= MY_LONG <= iss_longitude +5 and time_now_hour > sunset:
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(from_addr=my_email,
-                            to_addrs=my_email,
-                            msg=f"Subject:ISS is close by\n\nCheck the sky. Look up!")
-    print("email sent")
-else:
-    print("ISS is not close by")
-
 # BONUS: run the code every 60 seconds.
-
+while True:
+    if iss_latitude -5 <= MY_LAT <= iss_latitude +5 and iss_longitude -5 <= MY_LONG <= iss_longitude +5 and time_now_hour > sunset:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=my_email,
+                                msg=f"Subject:ISS is close by\n\nCheck the sky. Look up!")
+        print("email sent")
+    else:
+        print("ISS is not close by")
+    time.sleep(60)
