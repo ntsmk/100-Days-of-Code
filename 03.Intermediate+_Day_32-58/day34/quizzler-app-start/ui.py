8f64a4cd4f939af5d538cd019280a9d9f285ca6c
ntsmk <1991.ntm@gmail.com>
2024-11-14 12:27:16 -0700

    adding canvas

--- 03.Intermediate+_Day_32-58/day34/quizzler-app-start/ui.py
+++ 03.Intermediate+_Day_32-58/day34/quizzler-app-start/ui.py
@@ -8,7 +8,10 @@ class QuizInterface:
         self.window = Tk()
         self.window.title("Quizzler")
 
-        # todo set up UI here 6 grids - > look for reference in the past lesson day??
+        # todo set up UI here 6 grids - > look for reference
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        canvas = Canvas(width=300, height=250, bg="White")
+        canvas.grid(column=0, row=0)
 
 
         self.window.mainloop()
