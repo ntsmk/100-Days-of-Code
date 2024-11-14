01aa7b0279118a19d1a31504bfec903ab79ae777
ntsmk <1991.ntm@gmail.com>
2024-11-14 14:23:23 -0700

    adding image

--- 03.Intermediate+_Day_32-58/day34/quizzler-app-start/ui.py
+++ 03.Intermediate+_Day_32-58/day34/quizzler-app-start/ui.py
@@ -11,7 +11,14 @@ class QuizInterface:
         # todo set up UI here 6 grids - > look for reference
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         canvas = Canvas(width=300, height=250, bg="White")
-        canvas.grid(column=0, row=0)
+        canvas.grid(column=0, row=1, columnspan=2)
+
+        score = Label(text="Score:0")
+        score.grid(column=1, row=0)
+
+        true_button = PhotoImage(file="images/true.png")
+
+        false_button = PhotoImage(file="images/false.png")
 
 
         self.window.mainloop()
