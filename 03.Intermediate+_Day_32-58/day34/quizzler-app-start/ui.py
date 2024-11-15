ef2eac9ec27fcfd2ca5b809fdf8c08144f3d7721
ntsmk <1991.ntm@gmail.com>
2024-11-15 10:48:23 -0700

    adding more

--- 03.Intermediate+_Day_32-58/day34/quizzler-app-start/ui.py
+++ 03.Intermediate+_Day_32-58/day34/quizzler-app-start/ui.py
@@ -9,23 +9,22 @@ class QuizInterface:
 
         # todo set up the actual text and set up padding as well?
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.score = Label(text="Score:0", fg="white", bg=THEME_COLOR)
+        self.score.grid(column=1, row=0)
 
-        canvas = Canvas(width=300, height=250, bg="White")
-        canvas.grid(column=0, row=1, columnspan=2)
-        canvas.create_text(150, 125,
+        self.canvas = Canvas(width=300, height=250, bg="White")
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+        self.canvas.create_text(150, 125,
                            text="Dream is the most important. Even more important than actual life. Dream won’t die.",
                            font=("Arial", 20, "italic"), width=260)
 
-        score = Label(text="Score:0", fg="white", bg=THEME_COLOR)
-        score.grid(column=1, row=0)
+        self.true_button_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=self.true_button_img, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
 
-        true_button_img = PhotoImage(file="images/true.png")
-        true_button = Button(image=true_button_img, highlightthickness=0)
-        true_button.grid(column=0, row=2)
-
-        false_button_img = PhotoImage(file="images/false.png")
-        false_button = Button(image=false_button_img, highlightthickness=0)
-        false_button.grid(column=1, row=2)
+        self.false_button_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=self.false_button_img, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
 
 
         self.window.mainloop()
