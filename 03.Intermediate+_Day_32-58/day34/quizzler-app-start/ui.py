ae940255ce4b8658a5617456bdb2633bde984425
ntsmk <1991.ntm@gmail.com>
2024-11-15 11:26:18 -0700

    saving for now

--- 03.Intermediate+_Day_32-58/day34/quizzler-app-start/ui.py
+++ 03.Intermediate+_Day_32-58/day34/quizzler-app-start/ui.py
@@ -3,29 +3,32 @@ from tkinter import *
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
 
-        # todo set up the actual text and set up padding as well?
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         self.score = Label(text="Score:0", fg="white", bg=THEME_COLOR)
         self.score.grid(column=1, row=0)
 
         self.canvas = Canvas(width=300, height=250, bg="White")
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
-        self.canvas.create_text(150, 125,
+        self.canvas.create_text(150,
+                                125,
                            text="Dream is the most important. Even more important than actual life. Dream won’t die.",
                            font=("Arial", 20, "italic"), width=260)
 
-        self.true_button_img = PhotoImage(file="images/true.png")
-        self.true_button = Button(image=self.true_button_img, highlightthickness=0)
+        true_button_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_button_img, highlightthickness=0)
         self.true_button.grid(column=0, row=2)
 
-        self.false_button_img = PhotoImage(file="images/false.png")
-        self.false_button = Button(image=self.false_button_img, highlightthickness=0)
+        false_button_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_button_img, highlightthickness=0)
         self.false_button.grid(column=1, row=2)
 
 
         self.window.mainloop()
 
+    def get_next_question(self):
+        self.quiz.next_question
