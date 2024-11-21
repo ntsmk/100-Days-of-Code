2b35a481b674b3586e5fdb0722c7b3b45e590526
ntsmk <1991.ntm@gmail.com>
2024-11-21 16:46:46 -0700

    adding cnt param

--- 03.Intermediate+_Day_32-58/day35/rain_alert/main.py
+++ 03.Intermediate+_Day_32-58/day35/rain_alert/main.py
@@ -1,6 +1,7 @@
 import requests
 
 api_key = ""
+endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 MY_LAT = 60.721188
 MY_LONG = -135.056839
 
@@ -8,9 +9,11 @@ parameters = {
     "lat": MY_LAT,
     "lon": MY_LONG,
     "appid": api_key,
+    "cnt": 4,
 }
 
-response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response = requests.get(url=endpoint, params=parameters)
+response.raise_for_status()
 data = response.json()
 print(response.status_code)
 print(data)
