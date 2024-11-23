059a27fcf8b228bc0d4cbde2f16b771c5bd0d567
ntsmk <1991.ntm@gmail.com>
2024-11-22 17:11:27 -0700

    added rest

--- 03.Intermediate+_Day_32-58/day35/rain_alert/main.py
+++ 03.Intermediate+_Day_32-58/day35/rain_alert/main.py
@@ -4,12 +4,13 @@ api_key = ""
 endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 MY_LAT = 49.695596
 MY_LONG = -124.990431
+COUNT = 4
 
 parameters = {
     "lat": MY_LAT,
     "lon": MY_LONG,
     "appid": api_key,
-    "cnt": 4,
+    "cnt": COUNT,
 }
 
 response = requests.get(url=endpoint, params=parameters)
@@ -17,5 +18,12 @@ response.raise_for_status()
 data = response.json()
 print(response.status_code)
 print(data)
-# todo access the weather ID and print it out -> done, add if statement and print out bring umbrella
-print(data["list"][0]["weather"][0]["id"])
+
+condition_list = []
+for i in range(COUNT):
+    condition_list.append(data["list"][i]["weather"][0]["id"])
+# for i in condition_list:
+#     if i < 700:
+#         print("bring your umbrella")
+if any(x < 700 for x in condition_list):
+    print("bring your umbrella")
