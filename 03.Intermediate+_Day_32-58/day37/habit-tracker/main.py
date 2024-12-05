e6c4c3c294ed47123a12bff46bec7b64bb59a314
ntsmk <1991.ntm@gmail.com>
2024-12-05 12:09:52 -0700

    Adding more

--- 03.Intermediate+_Day_32-58/day37/habit-tracker/main.py
+++ 03.Intermediate+_Day_32-58/day37/habit-tracker/main.py
@@ -1,10 +1,31 @@
 import requests
 
+USERNAME = "whereismycoffee"
+TOKEN = ""
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_param = {
-    "token": "",
-    "username": "",
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+
 }
 
-requests.post()
+# response = requests.post(url=pixela_endpoint, json=user_param)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Walking Graph",
+    "unit": "steps",
+    "type": "int",
+    "color": "sora"
+
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config)
+print(response.text)
