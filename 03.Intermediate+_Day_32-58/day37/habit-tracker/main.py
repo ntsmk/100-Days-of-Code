17d461bf71154611ffefcc6c3cc350e462ef68a0
ntsmk <1991.ntm@gmail.com>
2024-12-06 12:47:11 -0700

    adding more

--- 03.Intermediate+_Day_32-58/day37/habit-tracker/main.py
+++ 03.Intermediate+_Day_32-58/day37/habit-tracker/main.py
@@ -1,7 +1,10 @@
 import requests
+from datetime import datetime
+
 
 USERNAME = "whereismycoffee"
-TOKEN = ""
+TOKEN = "gnAgjzIQxgVM"
+GRAPH_ID = "graph1"
 
 # creating account
 pixela_endpoint = "https://pixe.la/v1/users"
@@ -35,12 +38,25 @@ headers = {
 # print(response.text)
 
 # posting value to graph
-pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_config['id']}"
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime(year=2024, month=12, day=5)
+print(today.strftime("%Y%m%d"))
 
 pixel_config = {
-    "date": "20241205",
-    "quantity": "6500",
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "9000",
 }
 
-response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+# response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+# print(response.text)
+
+# updating value to graph
+update_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime("%Y%m%d")}"
+
+update_pixel_config = {
+    "quantity": "15000",
+}
+
+response = requests.put(url=update_pixel_endpoint, json=update_pixel_config, headers=headers)
 print(response.text)
