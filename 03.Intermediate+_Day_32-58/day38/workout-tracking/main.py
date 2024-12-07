b0182d87c6d252d9aca4c7652aa2c7353a7f98d1
Nat <1991.ntm@gmail.com>
2024-12-07 12:12:56 -0700

    adding more

--- 03.Intermediate+_Day_32-58/day38/workout-tracking/main.py
+++ 03.Intermediate+_Day_32-58/day38/workout-tracking/main.py
@@ -4,6 +4,7 @@ API_ID = ""
 API_KEY = ""
 
 api_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = "https://api.sheety.co/1105c27e55d43a710547b8fe3239700f/myWorkouts/workouts"
 
 headers = {
     'x-app-id': API_ID,
@@ -16,3 +17,7 @@ query = {
 
 response = requests.post(url=api_endpoint, headers=headers, data=query)
 print(response.json())
+
+# todo step 4 write some code to use the Sheety API to generate a new row of data in your Google Sheet
+# -> use POST
+# -> read documentation first
