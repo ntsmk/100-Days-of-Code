c0533356029e4412c9db8253f34c757a391c09c6
ntsmk <1991.ntm@gmail.com>
2024-12-09 14:20:04 -0700

    saving most of info in the sheet

--- 03.Intermediate+_Day_32-58/day38/workout-tracking/main.py
+++ 03.Intermediate+_Day_32-58/day38/workout-tracking/main.py
@@ -12,22 +12,27 @@ headers = {
     'x-app-key': API_KEY,
 }
 
-# query = {
-#     'query': input("tell me which exercise you did: ")
-# }
+query = {
+    'query': input("Tell me which exercise you did: ")
+}
 
-# response_nu = requests.post(url=nutrition_endpoint, headers=headers, data=query)
-# print(response_nu.json())
+response_nu = requests.post(url=nutrition_endpoint, headers=headers, data=query)
+# print(response_nu.json()["exercises"][0]["user_input"])
+
+today = datetime.today().strftime("%d/%m/%Y")
+exercise = response_nu.json()["exercises"][0]["user_input"].title()
+duration = response_nu.json()["exercises"][0]["duration_min"]
+calories = response_nu.json()["exercises"][0]["nf_calories"]
 
 # todo step 4 write some code to use the Sheety API to generate a new row of data in your Google Sheet
 # now I can add the raw by using dummy data so next step will be add the info from
 parameter = {
     'workout': {
-        'date': "21/07/2024",
+        'date': today,
         'time': "xx",
-        'exercise': "Running",
-        'duration': "xx",
-        'calories': "xx",
+        'exercise': exercise,
+        'duration': duration,
+        'calories': calories,
 
     }
 }
