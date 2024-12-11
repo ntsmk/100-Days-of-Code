1382a2b7487bdf0f48c1c2bd23762589a54a497b
ntsmk <1991.ntm@gmail.com>
2024-12-11 16:14:03 -0700

    adding more codes

--- 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/flight_search.py
+++ 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/flight_search.py
@@ -1,4 +1,18 @@
+import requests
+import os
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     # use Amadeus API
-    pass
+    API_KEY = os.environ["API_KEY"]
+    API_SECRET = os.environ["API_SECRET"]
+
+    amadeus_endpoint = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+
+    parameters = {
+        "originLocationCode": "YXY",
+        "destinationLocationCode": "TYO",
+        "departureDate": "2024-05-01",
+        "adults": 1
+    }
+    # response = requests.get(url=amadeus_endpoint, )
