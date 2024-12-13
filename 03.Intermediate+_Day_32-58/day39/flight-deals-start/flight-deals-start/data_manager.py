47da3534fef5b902b791d8e3f408806abb1f2879
ntsmk <1991.ntm@gmail.com>
2024-12-12 17:26:39 -0700

    adding more

--- 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/data_manager.py
+++ 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/data_manager.py
@@ -1,4 +1,15 @@
+import requests
+import os
+
+endpoint = "https://api.sheety.co/1105c27e55d43a710547b8fe3239700f/copyOfFlightDeals/prices"
+
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     # use Sheety API
-    pass
+    def getNames(self):
+        response = requests.get(url=endpoint)
+        names = []
+        for i in range(len(response.json()["prices"])):
+            names.append(response.json()["prices"][i]["city"])
+        return names
--- 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/flight_search.py
+++ 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/flight_search.py
@@ -2,7 +2,9 @@ import json
 
 import requests
 import os
+import data_manager
 
+# getting access token
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 oauth_endpoint = "https://test.api.amadeus.com/v1/security/oauth2/token"
@@ -17,22 +19,36 @@ client = {
 # response = requests.post(url=oauth_endpoint, headers=headers, data=client)
 # print(response.text)
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+auth_header = {
+    "Authorization": f"Bearer {ACCESS_TOKEN}"
+}
+
+# getting city IATA list
+city_search_endpoint = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+d = data_manager.DataManager()
+city_list = d.getNames()
+IATA_list = []
+for i in range(len(city_list)):
+    keyword = {
+        "keyword": city_list[i]
+    }
+    response = requests.get(url=city_search_endpoint, headers=auth_header, params=keyword)
+    IATA_list.append(response.json()["data"][0]["iataCode"])
+print(IATA_list)
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     # use Amadeus API
 
     amadeus_endpoint = "https://test.api.amadeus.com/v2/shopping/flight-offers"
-    auth_header = {
-        "Authorization": f"Bearer {ACCESS_TOKEN}"
-    }
+
     parameters = {
         "originLocationCode": "YXY",
-        "destinationLocationCode": "TYO",
+        "destinationLocationCode": "PAR",
         "departureDate": "2025-05-01",
         "adults": 1,
         "currencyCode": "CAD",
         "max": 10
     }
-    response = requests.get(url=amadeus_endpoint, headers=auth_header, params=parameters)
-    print(json.dumps(response.json()))
+    # response = requests.get(url=amadeus_endpoint, headers=auth_header, params=parameters)
+    # print(json.dumps(response.json()))
