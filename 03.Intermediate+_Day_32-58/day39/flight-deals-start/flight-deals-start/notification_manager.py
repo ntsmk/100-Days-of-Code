88f38210fd161de1f3b7013b42c52e080272cbb2
Nat <1991.ntm@gmail.com>
2024-12-14 14:19:53 -0700

    started editing notification module

--- 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/notification_manager.py
+++ 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/notification_manager.py
@@ -1,4 +1,17 @@
+import requests
+import os
+from twilio.rest import Client
+
+account_sid = os.getenv("account_sid")
+auth_token = os.getenv("auth_token")
+from_number = os.getenv("from_number")
+to_number = os.getenv("to_number")
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-    # use Twillio API
-    pass
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_=from_number,
+        body="test",
+        to=to_number
+    )
