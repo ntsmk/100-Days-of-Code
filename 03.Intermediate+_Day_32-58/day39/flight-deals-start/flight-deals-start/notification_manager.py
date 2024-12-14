c1688aac435929f03f9e0f56d029a49b2f08c4ea
Nat <1991.ntm@gmail.com>
2024-12-14 14:37:23 -0700

    editing more

--- 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/notification_manager.py
+++ 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/notification_manager.py
@@ -6,6 +6,8 @@ auth_token = os.getenv("auth_token")
 from_number = os.getenv("from_number")
 to_number = os.getenv("to_number")
 
+# todo edit "body", now it is just "test", import the data from "flight_search"
+# todo figure out if sentence in flight_search.py
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     client = Client(account_sid, auth_token)
@@ -14,3 +16,7 @@ class NotificationManager:
         body="test",
         to=to_number
     )
+
+    def sendText(self):
+        # later copy and paste here to give the access in flight_search
+        pass
