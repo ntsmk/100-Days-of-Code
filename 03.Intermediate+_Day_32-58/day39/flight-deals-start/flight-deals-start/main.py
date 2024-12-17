c38f6ee99c74438d1abc507275d66e01819451b6
ntsmk <1991.ntm@gmail.com>
2024-12-17 12:11:24 -0700

    adding more

--- 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/main.py
+++ 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/main.py
@@ -1,6 +1,9 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 
-from data_manager import DataManager
-from flight_search import FlightSearch
-from flight_data import FlightData
-from notification_manager import NotificationManager
+from step2 import DataManager
+
+dm = DataManager()
+sheet_data = dm.getEverything()
+print(sheet_data)
+
+
--- 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/step2.py
+++ 03.Intermediate+_Day_32-58/day39/flight-deals-start/flight-deals-start/step2.py
@@ -5,10 +5,21 @@ from dotenv import load_dotenv
 import os
 
 endpoint = "https://api.sheety.co/1105c27e55d43a710547b8fe3239700f/copyOfFlightDeals2/prices"
-
+put_endpoint = "https://api.sheety.co/1105c27e55d43a710547b8fe3239700f/copyOfFlightDeals2/prices/2"
 load_dotenv()
 auth = HTTPBasicAuth(os.getenv("UID1"), os.getenv("PASS1"))
 
-response = requests.get(url=endpoint, auth=auth)
-# print(response.json())
-pprint(response.json())
+class DataManager:
+
+    def getEverything(self):
+        response = requests.get(url=endpoint, auth=auth)
+        return response.json()
+
+    param = {
+        'price': {
+            'iataCode': "TESTING",
+        }
+    }
+
+    # response = requests.put(url=put_endpoint, json=param, auth=auth)
+    # print(response.text)
