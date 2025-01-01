d3fef618bd9e8a2e23c5cf5b936f30adcb7dc2f4
Nat <1991.ntm@gmail.com>
2025-01-01 11:20:11 -0700

    adding more

--- 03.Intermediate+_Day_32-58/day45/bs4-start/bs4-start/main.py
+++ 03.Intermediate+_Day_32-58/day45/bs4-start/bs4-start/main.py
@@ -8,4 +8,9 @@ with open("website.html") as file:
     content = file.read()
 
 soup = BeautifulSoup(content, "html.parser")
-print(soup.title)
+# print(soup.title)
+# print(soup.title.string)
+
+#print(soup.prettify())
+all_anchor_tags = soup.find_all(name="a")
+print(all_anchor_tags)
