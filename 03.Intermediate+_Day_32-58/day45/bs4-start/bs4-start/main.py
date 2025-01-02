fbdac744208b39dbb4dad3e9e772b14779da2ff2
Nat <1991.ntm@gmail.com>
2025-01-01 17:47:07 -0700

    adding more

--- 03.Intermediate+_Day_32-58/day45/bs4-start/bs4-start/main.py
+++ 03.Intermediate+_Day_32-58/day45/bs4-start/bs4-start/main.py
@@ -1,28 +1,45 @@
 from bs4 import BeautifulSoup
-# import lxml
+import requests
 
-# content = open("website.html", "r")
-# print(content.read())
+response = requests.get("https://news.ycombinator.com/news")
+# requests.get("https://appbrewery.github.io/news.ycombinator.com/")
 
-with open("website.html") as file:
-    content = file.read()
+print(response.text)
 
-soup = BeautifulSoup(content, "html.parser")
-# print(soup.title)
-# print(soup.title.string)
 
-#print(soup.prettify())
-all_anchor_tags = soup.find_all(name="a")
-# print(all_anchor_tags[0].getText())
-for tag in all_anchor_tags:
-    # print(tag.getText())
-    print(tag.get("href"))
 
-heading = soup.find(name="h1", id="name")
-print(heading)
-section_heading = soup.find(name="h3", class_="heading")
-print(section_heading.get("class"))
-name = soup.select_one(selector="#name")
-print(name)
-heading_a = soup.select(".heading")
-print(heading_a)
+
+
+
+
+
+
+
+
+# # import lxml
+#
+# # content = open("website.html", "r")
+# # print(content.read())
+#
+# with open("website.html") as file:
+#     content = file.read()
+#
+# soup = BeautifulSoup(content, "html.parser")
+# # print(soup.title)
+# # print(soup.title.string)
+#
+# #print(soup.prettify())
+# all_anchor_tags = soup.find_all(name="a")
+# # print(all_anchor_tags[0].getText())
+# for tag in all_anchor_tags:
+#     # print(tag.getText())
+#     print(tag.get("href"))
+#
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.get("class"))
+# name = soup.select_one(selector="#name")
+# print(name)
+# heading_a = soup.select(".heading")
+# print(heading_a)
