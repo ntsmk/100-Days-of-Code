e00d3d35c9d1b6744f89cb62176639ba2fafdbb9
Nat <1991.ntm@gmail.com>
2025-01-04 14:35:26 -0700

    finished challenge

--- 03.Intermediate+_Day_32-58/day45/Starting Code - 100 movies to watch start/main.py
+++ 03.Intermediate+_Day_32-58/day45/Starting Code - 100 movies to watch start/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
@@ -14,10 +15,15 @@ movie_list = []
 for title in titles:
     movie_list.append(title.getText())
 
-# todo figure out how to sort properly
-movie_list.sort()
-print(movie_list)
+# todo figure out how to sort properly -> found a solution
+# https://note.nkmk.me/en/python-sort-num-str/
+
+# movie_list.sort()
+# print(sorted(movie_list))
+# print(sorted(movie_list, key=lambda s: int(re.search(r'\d+', s).group())))
+movie_list1 = sorted(movie_list, key=lambda s: int(re.search(r'\d+', s).group()))
+# print(movie_list1)
 
 with open('movie.txt', 'w', encoding="utf-8") as f:
-    for i in range(len(movie_list)):
-        f.writelines(f"{movie_list[i]}\n")
+    for i in range(len(movie_list1)):
+        f.writelines(f"{movie_list1[i]}\n")
