9bb5ba6639ccd117d95984b8b54bbe42371fd006
Nat <1991.ntm@gmail.com>
2025-01-04 10:28:47 -0700

    scraping part is done

--- 03.Intermediate+_Day_32-58/day45/Starting Code - 100 movies to watch start/main.py
+++ 03.Intermediate+_Day_32-58/day45/Starting Code - 100 movies to watch start/main.py
@@ -4,6 +4,16 @@ from bs4 import BeautifulSoup
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
-print("this is test")
+response = requests.get(URL)
+movie_page = response.text
+soup = BeautifulSoup(movie_page, "html.parser")
 
+# titles = soup.find_all(class_="title")
+titles = soup.find_all("h3")
+movie_list = []
+for title in titles:
+    movie_list.append(title.getText())
 
+print(movie_list)
+
+# todo Generate a text file called movies.txt that lists the movie titles in ascending order (starting from 1)
