9876fcc6b3d5f08341be0ec5315782fb1bfd9db7
Nat <1991.ntm@gmail.com>
2025-01-04 10:47:42 -0700

    adding writing

--- 03.Intermediate+_Day_32-58/day45/Starting Code - 100 movies to watch start/main.py
+++ 03.Intermediate+_Day_32-58/day45/Starting Code - 100 movies to watch start/main.py
@@ -14,6 +14,12 @@ movie_list = []
 for title in titles:
     movie_list.append(title.getText())
 
+movie_list.sort()
 print(movie_list)
+
+with open('movie.txt', 'w') as f:
+    for i in range(len(movie_list)):
+        f.writelines(f"{movie_list[i]}\n")
+
 
 # todo Generate a text file called movies.txt that lists the movie titles in ascending order (starting from 1)
