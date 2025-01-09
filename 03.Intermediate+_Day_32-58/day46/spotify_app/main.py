e54c1694912ffed4e2eb70d73c3855529e91225f
ntsmk <1991.ntm@gmail.com>
2025-01-09 15:40:04 -0700

    Adding more

--- 03.Intermediate+_Day_32-58/day46/spotify_app/main.py
+++ 03.Intermediate+_Day_32-58/day46/spotify_app/main.py
@@ -3,36 +3,43 @@ from bs4 import BeautifulSoup
 import os
 from dotenv import load_dotenv
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+# from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
+user_id = os.getenv("USER_ID")
+redirect_uri = os.getenv("REDIRECT_URI")
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id,
-                                                           client_secret=client_secret))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret,
+                                               redirect_uri=redirect_uri, scope="playlist-modify-private"))
 
-# pulling the top 20 songs of the artist
-# results = sp.search(q='weezer', limit=20)
-# song_list = [song["name"] for song in results["tracks"]["items"]]
-# print(song_list)
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+URL = f"https://www.billboard.com/charts/hot-100/{date}/"
 
-# todo create a list of Spotify song URIs for the list of song names you found on Billboard
-results = sp.xxx # needs to figure it out
-print(results)
-
-
-# date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-#
-# URL = f"https://www.billboard.com/charts/hot-100/{date}/"
-# # URL = "https://www.billboard.com/charts/hot-100/2021-10-23/"
-#
+# 1. pulling the top 100 songs from the Billboard (Scraping part)
 # header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
 # response = requests.get(URL, headers=header)
 # billboard_page = response.text
 # soup = BeautifulSoup(billboard_page, "html.parser")
-#
 # songs_name = soup.select(selector="li h3", class_="c-title a-no-trucate")
 # song_list = [song.getText().strip() for song in songs_name]
-# song_list2 = song_list[0:100]
-# print(song_list2)
+# song_list_100 = song_list[0:100]
+# print(song_list_100)
+
+# 2. get URI for the song
+# todo need to figure out how to put itteraion for all the songs
+results = sp.search("Island In The Sun", type="track", limit=1)
+song_uri = results['tracks']['items'][0]['uri']
+
+# 3. create the new playlist
+playlist_creation = sp.user_playlist_create(user_id, f"{date} Billboard 100", public=False,
+                                            description=f"This is Billboard chart 100 on {date}")
+playlist_id = playlist_creation["id"]
+
+# 4. add the songs to the playlist
+# sp.user_playlist_add_tracks(user_id, playlist_id, song_uri, position=None)
+sp.user_playlist_add_tracks(user_id, playlist_id, [song_uri], position=None)
+
+print("Playlist created successfully!")
