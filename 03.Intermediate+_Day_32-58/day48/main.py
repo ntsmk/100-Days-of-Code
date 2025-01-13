a3993c031ca927d03cc3f07d35feb280f0ee4daf
ntsmk <1991.ntm@gmail.com>
2025-01-13 11:26:20 -0700

    started challenge

--- 03.Intermediate+_Day_32-58/day48/main.py
+++ 03.Intermediate+_Day_32-58/day48/main.py
@@ -1,11 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # keep Chrome open after program finishes
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("detach", True)
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.com")
+# driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Firefox()
+driver.get("https://www.python.org/")
+
+# price_dollor = driver.find_element(By.CLASS_NAME, "a-price-whole").text
+# print(f"The price is ${price_dollor}")
+
+# search_bar = driver.find_element(By.NAME, value="q")
+# print(search_bar.get_attribute("placeholder"))
+# button = driver.find_element(By.ID, value="submit")
+# print(button.size)
+# documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+# print(documentation_link.text)
+# bug_link = driver.find_element(By.XPATH, value="/html/body/div/footer/div[2]/div/ul/li[3]/a")
+# print(bug_link.text)
+# todo create a dictionary contains "Upcoming Events"
+events_time = driver.find_elements(By.CSS_SELECTOR, value="medium-widget event-widget last")
+print(events_time)
 
 # driver.close() -> just one tab
 # driver.quit() -> all tabs
