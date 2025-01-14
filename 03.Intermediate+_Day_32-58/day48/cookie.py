0a394d0d7dde5e69df42a1de27062fcbfbccd273
Nat <1991.ntm@gmail.com>
2025-01-13 19:35:42 -0700

    done

--- 03.Intermediate+_Day_32-58/day48/cookie.py
+++ 03.Intermediate+_Day_32-58/day48/cookie.py
@@ -6,24 +6,26 @@ driver = webdriver.Firefox()
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, value="cookie")
 
-timeout = 60   # [seconds]
+minutes = 5
+timeout = 60 * minutes  # [seconds]
 timeout_start = time.time()
+purchase_frequency = 7
 
-next_check = time.time() + 5
+next_check = time.time() + purchase_frequency
 while time.time() < timeout_start + timeout:
     cookie.click()
     money = int(driver.find_element(By.ID, value="money").text)
-    items = driver.find_elements(By.CSS_SELECTOR, value="#store div b")[:8]
+    items = driver.find_elements(By.CSS_SELECTOR, value="#store div b")[7::-1]
     if time.time() >= next_check:
         for item in items:
             item_cost = int(item.text.split("-")[1].replace(",", ""))
             if money > item_cost:
-                print(f"Clicking on item with cost: {item_cost}")
+                print(f"Clicking on {item.text.split("-")[0]} with cost: {item_cost}")
                 item.click()
                 break
-            else:
-                print(f"Not enough money: {money} for item cost: {item_cost}")
-        next_check = time.time() + 5
+            # else:
+                # print(f"Not enough money: {money} for item cost: {item_cost}")
+        next_check = time.time() + purchase_frequency
 
 cookie_second = driver.find_element(By.ID, value="cps").text
 driver.quit()
