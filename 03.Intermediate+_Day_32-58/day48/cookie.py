d8fb6489d03f7a7f041874a19752e1941dc34a3a
ntsmk <1991.ntm@gmail.com>
2025-01-13 17:07:44 -0700

    progress

--- 03.Intermediate+_Day_32-58/day48/cookie.py
+++ 03.Intermediate+_Day_32-58/day48/cookie.py
@@ -13,12 +13,12 @@ timeout_start = time.time()
 items = driver.find_elements(By.CSS_SELECTOR, value="#store div b")[:8]
 # item_cost = int(item.text.split("-")[1].replace(",", ""))
 
-while True:
-    cookie.click()
-    # money = int(driver.find_element(By.ID, value="money").text)
-    # item_cost = int(items[0].text.split("-")[1].replace(",", ""))
-    # if money > item_cost:
-    #     items[0].click()
+# while True:
+#     cookie.click()
+#     # money = int(driver.find_element(By.ID, value="money").text)
+#     # item_cost = int(items[0].text.split("-")[1].replace(",", ""))
+#     # if money > item_cost:
+#     #     items[0].click()
 
 
 
