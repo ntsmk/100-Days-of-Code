452401249307f347b919b2cf377c83d7a1f9ca18
Nat <1991.ntm@gmail.com>
2025-01-18 12:06:47 -0700

    completed step 2

--- 03.Intermediate+_Day_32-58/day49/linkedin_project/main.py
+++ 03.Intermediate+_Day_32-58/day49/linkedin_project/main.py
@@ -1,20 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import os
 from dotenv import load_dotenv
 import time
 
 URL = "https://www.linkedin.com/jobs/search/?currentJobId=4128218882&f_AL=true&keywords=python%20developer&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true"
 load_dotenv()
-email= os.getenv("email")
-password= os.getenv("password")
+email = os.getenv("email")
+password = os.getenv("password")
 
 driver = webdriver.Firefox()
 driver.get(URL)
 
-# todo need to figure out how to capture sign in button. Goal is Automatically Login to LinkedIn
-
-sign_in = driver.find_element(By.CSS_SELECTOR, "#sign-in-modal button")
 time.sleep(2)
-# print(sign_in)
+sign_in = driver.find_element(By.XPATH, "/html/body/div[5]/div/div/section/div/div/div/div[2]/button")
+time.sleep(2)
 sign_in.click()
+
+email_or_phone = driver.find_element(By.XPATH, '//*[@id="base-sign-in-modal_session_key"]')
+email_or_phone.send_keys(email)
+
+password_for_linkedin = driver.find_element(By.XPATH, '//*[@id="base-sign-in-modal_session_password"]')
+password_for_linkedin.send_keys(password)
+password_for_linkedin.send_keys(Keys.ENTER)
