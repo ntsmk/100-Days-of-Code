4a07dfd4c7fb2f9161c36bad3e781f339b8d1e59
Nat <1991.ntm@gmail.com>
2025-01-18 17:23:07 -0700

    in progress

--- 03.Intermediate+_Day_32-58/day49/linkedin_project/main.py
+++ 03.Intermediate+_Day_32-58/day49/linkedin_project/main.py
@@ -5,7 +5,12 @@ import os
 from dotenv import load_dotenv
 import time
 
-URL = "https://www.linkedin.com/jobs/search/?currentJobId=4128218882&f_AL=true&keywords=python%20developer&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true"
+URL = ("https://www.linkedin.com/jobs/search/"
+       "?currentJobId=4117936833"
+       "&f_AL=true"
+       "&keywords=python%20developer"
+       "&origin=JOB_SEARCH_PAGE_JOB_FILTER"
+       "&refresh=true")
 load_dotenv()
 email = os.getenv("email")
 password = os.getenv("password")
@@ -24,3 +29,23 @@ email_or_phone.send_keys(email)
 password_for_linkedin = driver.find_element(By.XPATH, '//*[@id="base-sign-in-modal_session_password"]')
 password_for_linkedin.send_keys(password)
 password_for_linkedin.send_keys(Keys.ENTER)
+
+# click save and follow
+time.sleep(5)
+# save = driver.find_element(By.CSS_SELECTOR, 'button.jobs-save-button:nth-child(3)')
+# save = driver.find_element(By.CSS_SELECTOR, 'button.jobs-save-button:nth-child(3)')
+# save.click() # it works after relocating again
+company = driver.find_element(By.CSS_SELECTOR, ".job-details-jobs-unified-top-card__company-name > a:nth-child(1)")
+company = driver.find_element(By.CSS_SELECTOR, ".job-details-jobs-unified-top-card__company-name > a:nth-child(1)")
+company = driver.find_element(By.CSS_SELECTOR, ".job-details-jobs-unified-top-card__company-name > a:nth-child(1)")
+company.click()
+time.sleep(2)
+
+follow = driver.find_element(By.CSS_SELECTOR, "button.follow:nth-child(1)")
+follow.click()
+# not_now = driver.find_element(By.CSS_SELECTOR, "#ember518")
+# not_now = driver.find_element(By.CSS_SELECTOR, "#ember518")
+# not_now.click()
+turn_on = driver.find_element(By.CSS_SELECTOR, "#ember522")
+# turn_on.click()
+driver.execute_script("arguments[0].click();", turn_on)
