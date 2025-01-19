25ef18577c33befe85dc60e5b780e3ddbc17062d
Nat <1991.ntm@gmail.com>
2025-01-19 12:34:47 -0700

    saving upto 7th

--- 03.Intermediate+_Day_32-58/day49/linkedin_project/main.py
+++ 03.Intermediate+_Day_32-58/day49/linkedin_project/main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import os
 from dotenv import load_dotenv
 import time
+from selenium.common.exceptions import StaleElementReferenceException
 
 URL = ("https://www.linkedin.com/jobs/search/"
        "?currentJobId=4117936833"
@@ -30,24 +31,34 @@ password_for_linkedin = driver.find_element(By.XPATH, '//*[@id="base-sign-in-mod
 password_for_linkedin.send_keys(password)
 password_for_linkedin.send_keys(Keys.ENTER)
 
-# click save and follow
-time.sleep(5)
-# save = driver.find_element(By.CSS_SELECTOR, 'button.jobs-save-button:nth-child(3)')
-# save = driver.find_element(By.CSS_SELECTOR, 'button.jobs-save-button:nth-child(3)')
-# save.click() # it works after relocating again
-company = driver.find_element(By.CSS_SELECTOR, ".job-details-jobs-unified-top-card__company-name > a:nth-child(1)")
-company = driver.find_element(By.CSS_SELECTOR, ".job-details-jobs-unified-top-card__company-name > a:nth-child(1)")
-company = driver.find_element(By.CSS_SELECTOR, ".job-details-jobs-unified-top-card__company-name > a:nth-child(1)")
-company.click()
-time.sleep(2)
+# # click save and follow
+# time.sleep(5)
+# # save = driver.find_element(By.CSS_SELECTOR, 'button.jobs-save-button:nth-child(3)')
+# # save = driver.find_element(By.CSS_SELECTOR, 'button.jobs-save-button:nth-child(3)')
+# # save.click() # it works after relocating again
+# company = driver.find_element(By.CSS_SELECTOR, ".job-details-jobs-unified-top-card__company-name > a:nth-child(1)")
+# company = driver.find_element(By.CSS_SELECTOR, ".job-details-jobs-unified-top-card__company-name > a:nth-child(1)")
+# company = driver.find_element(By.CSS_SELECTOR, ".job-details-jobs-unified-top-card__company-name > a:nth-child(1)")
+# company.click()
+# time.sleep(2)
+#
+# follow = driver.find_element(By.CSS_SELECTOR, "button.follow:nth-child(1)")
+# follow.click()
+# # not_now = driver.find_element(By.CSS_SELECTOR, "#ember518")
+# # not_now = driver.find_element(By.CSS_SELECTOR, "#ember518")
+# # not_now.click()
+# turn_on = driver.find_element(By.CSS_SELECTOR, "#ember522")
+# # turn_on.click()
+# driver.execute_script("arguments[0].click();", turn_on)
 
-follow = driver.find_element(By.CSS_SELECTOR, "button.follow:nth-child(1)")
-follow.click()
-# not_now = driver.find_element(By.CSS_SELECTOR, "#ember518")
-# not_now = driver.find_element(By.CSS_SELECTOR, "#ember518")
-# not_now.click()
-turn_on = driver.find_element(By.CSS_SELECTOR, "#ember522")
-# turn_on.click()
-driver.execute_script("arguments[0].click();", turn_on)
-
-# todo need to read Q&A section
+time.sleep(20) # because of security check
+jobs = driver.find_elements(By.CSS_SELECTOR, "div[class^='artdeco-entity-lockup__subtitle']")
+for i in range(7):
+    jobs[i].click()
+    try:
+       save = driver.find_element(By.CSS_SELECTOR, 'button.jobs-save-button:nth-child(3)')
+       save.click()
+    except StaleElementReferenceException:
+       save = driver.find_element(By.CSS_SELECTOR, 'button.jobs-save-button:nth-child(3)')
+       save.click()
+# todo need to get all job info
