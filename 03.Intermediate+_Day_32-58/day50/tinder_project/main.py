c930fa0ab34fb166f41154dd4056f9229c3fd653
Nat <1991.ntm@gmail.com>
2025-01-21 16:07:42 -0700

    getting close

--- 03.Intermediate+_Day_32-58/day50/tinder_project/main.py
+++ 03.Intermediate+_Day_32-58/day50/tinder_project/main.py
@@ -14,6 +14,7 @@ password = os.getenv("password")
 driver = webdriver.Firefox()
 driver.get(URL)
 
+# navigate to login
 time.sleep(5)
 login = driver.find_element(By.CSS_SELECTOR, "a.c1p6lbu0")
 login.click()
@@ -22,8 +23,32 @@ time.sleep(2)
 facebook_login = driver.find_element(By.CSS_SELECTOR, "button[aria-label^='Log in with Facebook']")
 facebook_login.click()
 
-email_input = driver.find_element(By.CSS_SELECTOR, "input[class^='email']") # need to modify
-email_input.send_keys(email)
+# change window and login with Facebook
+time.sleep(2)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
 
-password_input = driver.find_element(By.CSS_SELECTOR, "input[id='pass']") # need to modify
+email_input = driver.find_element(By.CSS_SELECTOR, "#email")
+email_input.send_keys(email)
+password_input = driver.find_element(By.CSS_SELECTOR, "#pass")
 password_input.send_keys(password)
+password_input.send_keys(Keys.ENTER)
+# need to do puzzle manually after this
+
+driver.switch_to.window(base_window)
+print(driver.title)
+
+# dismiss all requests
+time.sleep(5)
+allow = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div/div[3]/button[1]/div[2]/div[2]")
+allow.click()
+time.sleep(5)
+miss_out = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div/div[3]/button[2]/div[2]/div[2]")
+miss_out.click()
+
+# swipe left
+time.sleep(5)
+nope = driver.find_element(By.CSS_SELECTOR, ".Bgc\(\$c-ds-background-gamepad-sparks-nope-default\)")
+nope.click()
