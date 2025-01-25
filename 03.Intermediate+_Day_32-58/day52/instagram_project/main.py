2131b6f9da5c57c5ddf4786776459dc6599e8f92
Nat <1991.ntm@gmail.com>
2025-01-25 12:19:43 -0700

    adding class

--- 03.Intermediate+_Day_32-58/day52/instagram_project/main.py
+++ 03.Intermediate+_Day_32-58/day52/instagram_project/main.py
@@ -8,4 +8,24 @@ import time
 load_dotenv()
 username = os.getenv("username")
 password = os.getenv("password")
-similar_account = "chefsteps"
+similar_account = "nintendoamerica"
+
+instagram_url = "https://www.instagram.com/"
+
+class InstaFollower:
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+
+    def login(self):
+        pass
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
+
+insta_bot = InstaFollower()
+insta_bot.login()
+insta_bot.find_followers()
+insta_bot.follow()
