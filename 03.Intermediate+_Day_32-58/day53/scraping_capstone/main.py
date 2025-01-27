10e577adf15d5c58ee7a09145edc1e12b4ee938e
Nat <1991.ntm@gmail.com>
2025-01-26 20:04:19 -0700

    organizing tasks

--- 03.Intermediate+_Day_32-58/day53/scraping_capstone/main.py
+++ 03.Intermediate+_Day_32-58/day53/scraping_capstone/main.py
@@ -1,1 +1,7 @@
-# enter here
+# step 1
+# use beautifulsoup and scrape data (links, prices, address) and create the lists for each and then store it
+# https://appbrewery.github.io/Zillow-Clone/
+
+# step 2
+# use selenium and send data (price/address/link) for the form
+# https://docs.google.com/forms/d/e/1FAIpQLSfsjD62Gwntd50JDjRr-9_utIIG23_YwBGX2g_rc3V6CNTZOA/viewform
