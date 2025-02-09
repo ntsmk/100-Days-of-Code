2eb663058bb4ed7935387bef0213cd05998419c0
Nat <1991.ntm@gmail.com>
2025-02-09 16:11:26 -0700

    adding int

--- 03.Intermediate+_Day_32-58/day54/hello_flask/hello.py
+++ 03.Intermediate+_Day_32-58/day54/hello_flask/hello.py
@@ -2,9 +2,29 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return f"<em>{function()}</em>"
+    return wrapper
+
+def make_underline(function):
+    def wrapper():
+        return f"<u>{function()}</u>"
+    return wrapper
+
 @app.route("/")
+@make_underline # it needs to be under @app.route("/") to work
+@make_bold
+@make_emphasis
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return ("<h1 style='text-align: center'>Hello, World!</h1>"
+            "<p>this is a paragraph</p>"
+            "<img src='https://media.giphy.com/media/l0ExdMHUDKteztyfe/giphy.gif?cid=790b7611hxqze999001pbvqcopj7za9q432ajwpsusayqq8k&ep=v1_gifs_search&rid=giphy.gif&ct=g' alt='GIF'>")
 
 @app.route("/about")
 def about():
