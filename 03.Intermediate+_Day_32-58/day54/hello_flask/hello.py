9817bf4eccb0acf9a30428cd45ea03356155f491
Nat <1991.ntm@gmail.com>
2025-02-09 13:36:23 -0700

    adding int

--- 03.Intermediate+_Day_32-58/day54/hello_flask/hello.py
+++ 03.Intermediate+_Day_32-58/day54/hello_flask/hello.py
@@ -14,9 +14,9 @@ def about():
 def bye():
     return "<p>Goodbye!</p>"
 
-@app.route("/username/<name>")
-def greet(name):
-    return f"<p>Hello, {name}!</p>"
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"<p>Hello, {name + '12'}! How are you?? I am good. You are {number} years old</p>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True) # debug=True will automatically restart the server when changes are made to the code.
