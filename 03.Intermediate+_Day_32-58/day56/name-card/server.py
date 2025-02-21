050f4961dce80697d30401a12c93b6777a2a28d6
Nat <1991.ntm@gmail.com>
2025-02-21 15:40:03 -0700

    completed name card project

--- 03.Intermediate+_Day_32-58/day56/name-card/server.py
+++ 03.Intermediate+_Day_32-58/day56/name-card/server.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def greet():
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
