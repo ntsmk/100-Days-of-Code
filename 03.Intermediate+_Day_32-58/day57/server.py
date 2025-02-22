4c5afae845d3b166663ea8db54e2bd592c864e23
Nat <1991.ntm@gmail.com>
2025-02-22 12:47:54 -0700

    finishing challenge

--- 03.Intermediate+_Day_32-58/day57/server.py
+++ 03.Intermediate+_Day_32-58/day57/server.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import random
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    random_number = random.randint(1,10)
+    my_name = "Nat"
+    current_year = datetime.now().year
+    return render_template("index.html", num=random_number, my_name=my_name, current_year=current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ 03.Intermediate+_Day_32-58/day57/test.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+current_year = datetime.now().year
+
+print(curent_year)
