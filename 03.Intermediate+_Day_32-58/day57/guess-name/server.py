f782f674b3b39aeedbb65c16656c000e7353d68b
Nat <1991.ntm@gmail.com>
2025-02-23 10:00:28 -0700

    progress in challenge

--- 03.Intermediate+_Day_32-58/day57/guess-name/server.py
+++ 03.Intermediate+_Day_32-58/day57/guess-name/server.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
+import requests
+
+age_endpoint = "https://api.agify.io?name="
+gender_endpoint = "https://api.genderize.io?name="
+# response_age = requests.get(url=age_endpoint+"natsumi")
+# response_gender = requests.get(url=gender_endpoint+"natsumi")
+# print(response_age.json()['age']) # 38
+# print(response_age.json()['name'].title()) # capitalized first letter\
+# print(response_gender.json()['gender']) # female
 
 app = Flask(__name__)
-
 @app.route('/')
-def home():
+def display():
+    return '<h1>Enter your name</h1>'
+
+@app.route('/guess/<str:entered_name>')
+def result(entered_name):
     # todo need to fill out here. use agefy and genderfy API. take input from URL path
-    name = # todo capitalize it
-    gender =
-    age =
+    name = entered_name.title()
+    gender = requests.get(url=gender_endpoint+name).json()['gender']
+    age = requests.get(url=age_endpoint+name).json()['age']
     return render_template("index.html", name=name, gender=gender, age=age)
 
 if __name__ == "__main__":
