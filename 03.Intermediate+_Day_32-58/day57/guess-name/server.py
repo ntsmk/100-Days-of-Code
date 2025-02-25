18fb56cb34554ffe87285537c65e70aa62325d7e
Nat <1991.ntm@gmail.com>
2025-02-24 21:55:24 -0700

    adding num

--- 03.Intermediate+_Day_32-58/day57/guess-name/server.py
+++ 03.Intermediate+_Day_32-58/day57/guess-name/server.py
@@ -21,5 +21,13 @@ def result(entered_name):
     age = requests.get(url=age_endpoint+name).json()['age']
     return render_template("index.html", name=name, gender=gender, age=age)
 
+@app.route('/blog/<num>')
+def get_blog(num):
+    print(num)
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_post = response.json()
+    return render_template("blog.html", posts=all_post)
+
 if __name__ == "__main__":
     app.run(debug=True)
