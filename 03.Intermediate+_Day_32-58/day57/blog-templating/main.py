cd23be323957264290b461ff7926e4105a98c735
ntsmk <1991.ntm@gmail.com>
2025-02-25 15:36:52 -0700

    finished challenge

--- 03.Intermediate+_Day_32-58/day57/blog-templating/main.py
+++ 03.Intermediate+_Day_32-58/day57/blog-templating/main.py
@@ -3,18 +3,17 @@ import requests
 
 
 app = Flask(__name__)
+blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+response = requests.get(blog_url)
+all_post = response.json()
 
 @app.route('/')
 def home():
-    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
-    response = requests.get(blog_url)
-    all_post = response.json()
     return render_template("index.html", posts=all_post)
-
 
 @app.route('/post/<num>')
 def get_post(num):
-    return render_template('post.html')
+    return render_template('post.html', post=all_post[int(num)-1])
 
 if __name__ == "__main__":
     app.run(debug=True)
